3d2d5bdcb0bb7ea9b444069041f2101e6eaacda7
YongYong21 <imkinggosu@gmail.com>
2023-11-06 18:23:18 +0900

    Add: GolbalStyle, theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { GlobalStyle } from "./GlobalStyle.ts";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
