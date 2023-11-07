b0c23b7a449e43b34d2a35aefbc7acbd0a7da591
JiHongkyu <hongkyu9941@gmail.com>
2023-11-07 13:15:48 +0900

    live server test

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Editeded <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
