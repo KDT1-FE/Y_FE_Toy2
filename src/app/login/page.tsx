8721c460ef381e460acdd169e4d3df18288a0725
NohWookJin <jinn98@kakao.com>
2023-11-07 17:16:36 +0900

    Feat: 임시 세션 처리

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// components
+import LoginForm from '@/components/Login/LoginForm';
+
+const page = () => {
+    return (
+        <div>
+            <LoginForm />
+        </div>
+    );
+};
+
+export default page;
