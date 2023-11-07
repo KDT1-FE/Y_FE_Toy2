c0f19c400e84fb235080a208f2134c3b66c8d061
Yonghun <yonghuni484@gmail.com>
2023-11-07 21:19:35 +0900

    Env: Install query devtools and setting

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,18 @@ import App from "./App.tsx";
 import "./index.css";
 import theme from "./theme/index.ts";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={true} />
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
 );
