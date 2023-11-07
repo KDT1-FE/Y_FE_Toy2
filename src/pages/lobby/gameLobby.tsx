d7240aa66c1875463b66720e4de819d1e3115a5e
yangjaehyuk <gndksakxk10@naver.com>
2023-11-07 11:14:04 +0900

    Chore: 로비 페이지 초기 세팅

--- src/pages/lobby/gameLobby.tsx
+++ src/pages/lobby/gameLobby.tsx
@@ -1,5 +1,21 @@
+import React from 'react';
+import { getAllUsers } from '../../api/index';
 const GameLobby = () => {
-  return <div style={{ padding: 20, textAlign: 'center' }}>로비</div>;
+  const token: string | null = localStorage.getItem('jwt');
+
+  const handleGetAllUsers = () => {
+    if (token) {
+      getAllUsers(token);
+    } else {
+      alert('error');
+    }
+  };
+
+  return (
+    <>
+      <button onClick={handleGetAllUsers}>getAllUsers</button>
+    </>
+  );
 };
 
 export default GameLobby;
