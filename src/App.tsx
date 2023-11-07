f26cfd0bc6091c27d5409b1b1e64b3d8d588c379
YongYong21 <imkinggosu@gmail.com>
2023-11-07 10:37:05 +0900

    Add: PageNotFound

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
+
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<div>메인</div>} />
+      <Route path="*" element={<PageNotFound></PageNotFound>} />
+    </Routes>
+  );
 }
 
 export default App;
