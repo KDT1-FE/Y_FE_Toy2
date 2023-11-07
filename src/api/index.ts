55664ad74756cce8fe85cbf30972bef4a3b05345
Hojin <wkdghwls0623@gmail.com>
2023-11-08 02:43:13 +0900

    Feat: 로그인, 회원가입 구현

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,63 +1,33 @@
 import axios from 'axios';
-import { SERVER_URL, CONTENT_TYPE, SERVER_ID } from '../constant';
+import { CONTENT_TYPE, SERVER_ID, SERVER_URL } from '../constant';
+import { JoinData } from '../interfaces/interface';
 
-export const signup = async (
-  id: string,
-  password: string,
-  name: string,
-  picture?: string,
-) => {
-  const authData = {
-    id,
-    password,
-    name,
-    picture,
-  };
-  console.log(authData);
+const client = axios.create({
+  baseURL: SERVER_URL,
+  headers: {
+    'content-type': CONTENT_TYPE,
+    serverId: SERVER_ID,
+  },
+});
 
-  try {
-    return await axios.post(`${SERVER_URL}/signup`, authData, {
-      headers: {
-        'content-type': CONTENT_TYPE,
-        serverId: SERVER_ID,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-  }
+export const postLogin = async (id: string, password: string) => {
+  const res = await client.post('/login', {
+    id: id,
+    password: password,
+  });
+  return res;
 };
 
-export const login = async (id: string, password: string) => {
-  const authData = {
-    id,
-    password,
-  };
-  try {
-    const response = await axios.post(`${SERVER_URL}/login`, authData, {
-      headers: {
-        'content-type': CONTENT_TYPE,
-        serverId: SERVER_ID,
-      },
-    });
-    const { accessToken } = response.data;
-    localStorage.setItem('jwt', accessToken);
-    console.log('Login successful');
-  } catch (error) {
-    console.error('Login failed:', error);
-  }
+export const postJoin = async (joinData: JoinData) => {
+  const res = await client.post('/signup', joinData);
+  return res;
 };
 
 export const getAllUsers = async (accessToken: string) => {
-  try {
-    const response = await axios.get(`${SERVER_URL}/users`, {
-      headers: {
-        'content-type': CONTENT_TYPE,
-        serverId: SERVER_ID,
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    console.log(response.data);
-  } catch (error) {
-    console.log(error);
-  }
+  const res = await client.get('/users', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  return res;
 };
--- src/pages/lobby/gameLobby.tsx
+++ src/pages/lobby/gameLobby.tsx
@@ -1,12 +1,22 @@
 import { getAllUsers } from '../../api/index';
 const GameLobby = () => {
-  const token: string | null = localStorage.getItem('jwt');
+  const handleGetAllUsers = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-  const handleGetAllUsers = () => {
-    if (token) {
-      getAllUsers(token);
-    } else {
-      alert('error');
+    const token = localStorage.getItem('jwt');
+    if (!token) {
+      alert('인증 토큰이 없습니다. 로그인이 필요합니다.');
+      return;
+    }
+
+    try {
+      // 닉네임 중복 핸들링 로직 필요
+      const res = await getAllUsers(token);
+      console.log(res.data);
+      alert('성공');
+    } catch (e: any) {
+      console.error('에러:', e.response || e);
+      alert('에러 발생');
     }
   };
 
