c227ba2b873187a0de7e9528a3c1ba23e60f9815
YongYong21 <imkinggosu@gmail.com>
2023-11-07 17:32:56 +0900

    Add: Pretendard font

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -3,8 +3,13 @@ import reset from "styled-reset";
 
 export const GlobalStyle = createGlobalStyle`
     ${reset}
+    @font-face {
+        font-family: 'Pretendard';
+        src: url('../assets/fonts/PretendardVariable.woff2');
+    }
     body {
         background-color: ${({ theme }) => theme.bgColor};
         color: ${({ theme }) => theme.textColor};
+        font-family: 'Pretendard'
     }
 `;
