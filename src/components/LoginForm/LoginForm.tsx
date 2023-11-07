14d3facc73e039a514de71eeed18fc15cd7c9a11
Taegwan Youn <tkyoun0421@naver.com>
2023-11-07 20:11:39 +0900

    Feat: Finish SignUp page

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,24 +1,8 @@
 import styled from "styled-components";
-import FormInput from "../FormInput/FormInput";
+import FormInput from "../SignUpInput/SignUpInput";
 
 export const LoginForm = () => {
-  return (
-    <LoginContainer>
-      <FormInput
-        id={"id"}
-        label={"아이디"}
-        inputProps={{ type: "text", placeholder: "아이디를 입력해주세요." }}
-      />
-      <FormInput
-        id={"pw"}
-        label={"비밀번호"}
-        inputProps={{
-          type: "password",
-          placeholder: "비밀번호를 입력해주세요."
-        }}
-      />
-    </LoginContainer>
-  );
+  return <LoginContainer></LoginContainer>;
 };
 
 const LoginContainer = styled.form`
--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,17 +1,38 @@
 import styled from "styled-components";
-import FormInput from "../FormInput/FormInput";
+import FormInput from "../SignUpInput/SignUpInput";
 import { FormInputBtn } from "../FormInputBtn/FormInputBtn";
+import { useState } from "react";
 
-export const SignUpForm = () => {
+const initialErrorData: ErrorDataType = {
+  id: "",
+  name: "",
+  pw: "",
+  confirmPw: ""
+};
+
+const initialFormData = {
+  id: "",
+  name: "",
+  pw: "",
+  confirmPw: ""
+};
+
+function SignUpForm() {
+  const [errorData, setErrorData] = useState(initialErrorData);
+  const [formData, setFormData] = useState(initialFormData);
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("#");
   };
+
   return (
     <SignUpContainer onSubmit={handleSubmit}>
       <FormInput
         id={"id"}
         label={"아이디"}
+        formData={formData}
+        setFormData={setFormData}
+        errorData={errorData}
+        setErrorData={setErrorData}
         inputProps={{
           type: "text",
           placeholder: "영어로 입력해주세요."
@@ -20,6 +41,10 @@ export const SignUpForm = () => {
       <FormInput
         id={"name"}
         label={"이름"}
+        formData={formData}
+        setFormData={setFormData}
+        errorData={errorData}
+        setErrorData={setErrorData}
         inputProps={{
           type: "text",
           placeholder: "이름을 입력해주세요."
@@ -28,14 +53,22 @@ export const SignUpForm = () => {
       <FormInput
         id={"pw"}
         label={"비밀번호"}
+        formData={formData}
+        setFormData={setFormData}
+        errorData={errorData}
+        setErrorData={setErrorData}
         inputProps={{
           type: "password",
-          placeholder: "5글자 이상 입력해주세요."
+          placeholder: "8~16자 영문 대 소문자, 숫자를 입력해주세요."
         }}
       />
       <FormInput
         id={"confirmPw"}
         label={"비밀번호 확인"}
+        formData={formData}
+        setFormData={setFormData}
+        errorData={errorData}
+        setErrorData={setErrorData}
         inputProps={{
           type: "password",
           placeholder: "비밀번호와 동일하게 입력해주세요."
@@ -44,7 +77,38 @@ export const SignUpForm = () => {
       <FormInputBtn value={"완료"} />
     </SignUpContainer>
   );
-};
+}
+
+export default SignUpForm;
+
+export interface FormInputPropsType {
+  id: string;
+  label: string;
+  formData: FormDataType;
+  setFormData: (data: FormDataType) => void;
+  errorData: ErrorDataType;
+  setErrorData: (error: ErrorDataType) => void;
+  inputProps: {
+    type: string;
+    placeholder: string;
+  };
+}
+
+interface FormDataType {
+  id: string;
+  name: string;
+  pw: string;
+  confirmPw: string;
+  [key: string]: string;
+}
+
+export interface ErrorDataType {
+  id: string;
+  name: string;
+  pw: string;
+  confirmPw: string;
+  [key: string]: string | true;
+}
 
 const SignUpContainer = styled.form`
   display: flex;
