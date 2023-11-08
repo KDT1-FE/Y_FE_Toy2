1dc2ba95294b789da38fce79b7292477e1df330a
JitHoon <imessi0336@gmail.com>
2023-11-08 22:20:01 +0900

    Feat: 오픈 채팅방 검색 페이지 생성

---
+++ app/search/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { fetchAllOpenChat } from './search.utils';
+
+const accessToken = process.env.ACCESSTOKEN as string; // 임시 access token
+
+const Search = async () => {
+	const allOpenChat = await fetchAllOpenChat(accessToken);
+	console.log(allOpenChat);
+
+	return (
+		<>
+			<h1>Search 페이지</h1>
+		</>
+	);
+};
+
+export default Search;
