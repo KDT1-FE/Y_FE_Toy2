8fb52aa05e9239f1e1c1df20b48b87dadcc7c89b
JitHoon <imessi0336@gmail.com>
2023-11-08 22:20:39 +0900

    Feat: 모든 오픈 채팅방 목록을 불러오는 비동기 함수 구현

---
+++ app/search/search.utils.ts
@@ -0,0 +1,18 @@
+import { Request, AllOpenChat } from './search.type';
+import { GET, CONTENT_TYPE, BASE_URL, GET_ALL_CHAT } from './search.constant';
+
+export const fetchAllOpenChat = async (accessToken: string) => {
+	const Request: Request = {
+		method: GET,
+		headers: {
+			'content-type': CONTENT_TYPE,
+			serverId: process.env.SERVER_ID as string, // 서버 아이디 임시 사용
+			Authorization: `Bearer ${accessToken}`,
+		},
+	};
+
+	const res = await fetch(`${BASE_URL}${GET_ALL_CHAT}`, Request);
+	const allOpenChat: AllOpenChat = await res.json();
+
+	return allOpenChat;
+};
