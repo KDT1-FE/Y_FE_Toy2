d8f493ba74efd9e029d6eaa0316c0d5692d882a6
Hojin <wkdghwls0623@gmail.com>
2023-11-09 02:20:08 +0900

    Feat: 토큰재발급 구현완료

--- src/pages/lobby/gameLobby.tsx
+++ src/pages/lobby/gameLobby.tsx
@@ -3,12 +3,16 @@ import CheckGameRoom from '../../components/layout/checkGameRoom';
 // import OnlineUserList from '../../components/layout/onineUserList';
 // import UserList from '../../components/layout/userList';
 import { getAllUsers } from '../../api';
+import { postRefresh } from '../../api';
 import { Button } from '@chakra-ui/react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { accessTokenState } from '../../states/atom';
+import { useNavigate } from 'react-router-dom';
 
 const GameLobby = () => {
-  const accessToken = useRecoilValue(accessTokenState);
+  const accessToken = useRecoilValue(accessTokenState); // hook 규칙으로 함수 외부에있어야함
+  const setAccessToken = useSetRecoilState(accessTokenState); // hook 규칙으로 함수 외부에있어야함
+  const navigate = useNavigate();
 
   const handleGetAllUsers = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -22,8 +26,29 @@ const GameLobby = () => {
     }
   };
 
+  const handlePostRefresh = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const refreshToken = localStorage.getItem('refreshToken');
+    if (refreshToken === null) {
+      alert('로그인이 만료되었습니다.');
+      navigate('/');
+      return;
+    }
+
+    try {
+      const res = await postRefresh(refreshToken);
+      setAccessToken(res.data.accessToken);
+      alert('토큰 재발급에 성공했습니다.');
+    } catch (e) {
+      console.error(e);
+      alert('토큰 재발급에 실패했습니다.');
+    }
+  };
+
   return (
     <>
+      <Button onClick={handlePostRefresh}>토큰재발급</Button>
       <Button onClick={handleGetAllUsers}>모든유저보기</Button>
       {/* <UserList></UserList> */}
       <br></br>
