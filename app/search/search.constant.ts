042f4b7e68c8e35105351ef50a53b5235748fb58
JitHoon <imessi0336@gmail.com>
2023-11-08 22:21:03 +0900

    Feat: 모든 오픈 채팅방 목록 불러오기 함수 상수값 설정

---
+++ app/search/search.constant.ts
@@ -0,0 +1,9 @@
+// http Method
+export const GET = 'GET';
+
+// API URL
+export const BASE_URL = 'https://fastcampus-chat.net/';
+export const GET_ALL_CHAT = 'chat/all';
+
+// API Request 조건 정보
+export const CONTENT_TYPE = 'application/json';
