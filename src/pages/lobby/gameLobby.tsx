ffb38dd1128afce9808378e8fb6fdb82d35bae3c
Hojin <wkdghwls0623@gmail.com>
2023-11-09 02:44:38 +0900

    Feat: 토큰재발급, 모든유저보기 핸들러 함수화해서 util.ts 로 이동

--- src/pages/lobby/gameLobby.tsx
+++ src/pages/lobby/gameLobby.tsx
@@ -2,54 +2,20 @@ import CreateGameRoom from '../../components/layout/createGameRoom';
 import CheckGameRoom from '../../components/layout/checkGameRoom';
 // import OnlineUserList from '../../components/layout/onineUserList';
 // import UserList from '../../components/layout/userList';
-import { getAllUsers } from '../../api';
-import { postRefresh } from '../../api';
 import { Button } from '@chakra-ui/react';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { handleGetAllUsers } from '../../util/util';
+import { handlePostRefresh } from '../../util/util';
 import { accessTokenState } from '../../states/atom';
-import { useNavigate } from 'react-router-dom';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 const GameLobby = () => {
-  const accessToken = useRecoilValue(accessTokenState); // hook 규칙으로 함수 외부에있어야함
   const setAccessToken = useSetRecoilState(accessTokenState); // hook 규칙으로 함수 외부에있어야함
-  const navigate = useNavigate();
 
-  const handleGetAllUsers = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    try {
-      const res = await getAllUsers(accessToken);
-      console.log(res);
-    } catch (e) {
-      console.error(e);
-      alert('사용자 정보를 가져오는데 실패했습니다.');
-    }
-  };
-
-  const handlePostRefresh = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    const refreshToken = localStorage.getItem('refreshToken');
-    if (refreshToken === null) {
-      alert('로그인이 만료되었습니다.');
-      navigate('/');
-      return;
-    }
-
-    try {
-      const res = await postRefresh(refreshToken);
-      setAccessToken(res.data.accessToken);
-      alert('토큰 재발급에 성공했습니다.');
-    } catch (e) {
-      console.error(e);
-      alert('토큰 재발급에 실패했습니다.');
-    }
-  };
-
+  const accessToken = useRecoilValue(accessTokenState);
   return (
     <>
-      <Button onClick={handlePostRefresh}>토큰재발급</Button>
-      <Button onClick={handleGetAllUsers}>모든유저보기</Button>
+      <Button onClick={handlePostRefresh(setAccessToken)}>토큰재발급</Button>
+      <Button onClick={handleGetAllUsers(accessToken)}>모든유저보기</Button>
       {/* <UserList></UserList> */}
       <br></br>
       {/* <OnlineUserList /> */}
