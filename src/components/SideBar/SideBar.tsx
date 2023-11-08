a21fec3911c304b89d295b7c08053f0ea1b06701
SKY-PEY <lovr8537@gmail.com>
2023-11-08 16:32:57 +0900

    design: 사이드바 레이아웃 초기 설정

---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div>
+      <div>로고자리</div>
+      <div>
+        <ul>
+        <li>전체 채팅방 조회</li>
+        <li>채팅 생성하기</li>
+        <li>개인 정보 수정</li>
+        </ul>
+        <h2>나의 채팅방</h2>
+        <ul>
+          <li>채팅방1 예시</li>
+        </ul>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default SideBar
