e0feafffd16d67f149907d997b931e0bdc3f150b
Hojin <wkdghwls0623@gmail.com>
2023-11-09 03:02:22 +0900

    Fix: 라우터구조 변경

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,4 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
+import UserLogin from '../pages/login/userLogin';
+import UserJoin from '../pages/login/userJoin';
 import GameLobby from '../pages/lobby/gameLobby';
 import GameRoom from '../pages/room/gameRoom';
 
@@ -6,6 +8,8 @@ const MainRouter = () => {
   return (
     <>
       <Routes>
+        <Route path="/" element={<UserLogin />} />
+        <Route path="/join" element={<UserJoin />} />
         <Route path="/lobby" element={<GameLobby />} />
         <Route path="/room/:id" element={<GameRoom />} />
       </Routes>
