b1d6505eef87a08d7a61188fd45760f693ed4fc6
sue <dldustn2040@gmail.com>
2023-11-09 17:12:01 +0900

    Feat: Add Keyword Modal

--- src/components/Game/Keyword/index.tsx
+++ src/components/Game/Keyword/index.tsx
@@ -1,42 +1,105 @@
+import {
+  Box,
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 import data from "../../../data/category.json";
 
+interface Categories {
+  category: string;
+  keyword: string[];
+}
+[];
+
+const Div = styled.div`
+  margin: 20rem;
+`;
+
 const Keyword = () => {
+  const categories = data.CategoryList;
+  const [category, setCategory] = useState<Categories | null>(null);
+  const [keyword, setKeyword] = useState("");
+
+  const users = data.users;
+  const [liar, setLiar] = useState("");
+  const [isLiar, setIsLiar] = useState(false);
+
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (isOpen) {
+      timer = setTimeout(() => {
+        onClose();
+      }, 2000);
+    }
+    return () => clearTimeout(timer);
+  }, [isOpen, onClose]);
+
   const getRandNum = (length: number): number => {
     const randNum = Math.floor(Math.random() * length);
 
     return randNum;
   };
 
-  const categories = data.CategoryList;
-  const category = categories[getRandNum(categories.length)];
-  const keyword = category.keyword[getRandNum(category.keyword.length)];
+  const handleStart = () => {
+    onOpen();
 
-  const users = data.users;
-  const liar = users.name[getRandNum(users.name.length)];
+    const selectedCategory = categories[getRandNum(categories.length)];
+    setCategory(selectedCategory);
+    setKeyword(
+      selectedCategory.keyword[getRandNum(selectedCategory.keyword.length)],
+    );
 
-  const myName = "연수";
-
-  const [isLiar, setIsLiar] = useState(false);
-
-  useEffect(() => {
-    if (liar === myName) {
+    setLiar(users.name[getRandNum(users.name.length)]);
+    if (liar === "연수") {
       setIsLiar(true);
-    }
-    return () => {
+    } else {
       setIsLiar(false);
-    };
-  }, [liar]);
+    }
+  };
 
   return (
-    <div>
-      <p>카테고리: {category.category}</p>
-      {isLiar ? (
-        <p>당신은 Liar 입니다. 키워드를 추리하세요.</p>
-      ) : (
-        <p>키워드: {keyword}</p>
-      )}
-    </div>
+    <Div>
+      <Button onClick={handleStart}>게임시작</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent width={420} height={375}>
+          <ModalCloseButton />
+          <ModalBody>
+            <p>주제는 {category?.category} 입니다.</p>
+            {isLiar ? (
+              <p>당신은 Liar 입니다. 키워드를 추리하세요.</p>
+            ) : (
+              <p>키워드는 {keyword} 입니다.</p>
+            )}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+      <Box>
+        {category && (
+          <>
+            <p>주제는 {category?.category} 입니다.</p>
+            {isLiar ? (
+              <>
+                <p>당신은 Liar 입니다.</p>
+                <p>키워드를 추리하세요.</p>
+              </>
+            ) : (
+              <p>키워드는 {keyword} 입니다.</p>
+            )}
+          </>
+        )}
+      </Box>
+    </Div>
   );
 };
 
