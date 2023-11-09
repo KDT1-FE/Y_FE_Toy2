9599e742711db377740879f2c65fee178ab3d5be
Yonghun <yonghuni484@gmail.com>
2023-11-09 15:27:29 +0900

    Feat: Get game info

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -1,4 +1,15 @@
+import useFireFetch from "../../hooks/useFireFetch";
+
 const Game = () => {
+  const queryString = window.location.search;
+  const searchParams = new URLSearchParams(queryString);
+  const gameId = searchParams.get("gameId");
+
+  const fireFetch = useFireFetch();
+
+  const gameData = fireFetch.useGetSome("game", "id", gameId as string);
+
+  console.log(gameData.data);
   return <div>game page</div>;
 };
 
