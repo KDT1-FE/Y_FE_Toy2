0939ddf53db1a7aa676c1cb78e4c1346d1c90c90
hhjs2 <0092h@naver.com>
2023-11-10 02:32:33 +0900

    Fix: RequestBody 타입 수정

--- Components/Join/JoinForm.tsx
+++ Components/Join/JoinForm.tsx
@@ -8,7 +8,7 @@ type RequestBody = {
 	id: string; // 사용자 아이디 (필수!, 영어와 숫자만)
 	password: string; // 사용자 비밀번호, 5자 이상 (필수!)
 	name: string; // 사용자 이름, 20자 이하 (필수!)
-	picture: string; // 사용자 이미지(url or base64, under 1MB)
+	picture: FileList; // 사용자 이미지(url or base64, under 1MB)
 	// image: string;
 };
 
@@ -29,6 +29,8 @@ const JoinForm = () => {
 	useEffect(() => {
 		if (image && image.length > 0) {
 			const file = image[0];
+			console.log(image);
+			console.log(file);
 			console.log(URL.createObjectURL(file));
 			setImagePreview(URL.createObjectURL(file));
 		}
