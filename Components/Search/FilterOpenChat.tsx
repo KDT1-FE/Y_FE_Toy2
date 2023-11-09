b30b399d3dd0735494b3041a6f8c634e8b048139
JitHoon <imessi0336@gmail.com>
2023-11-09 16:30:21 +0900

    Feat: Search 페이지에 추가할 컴포넌트들 추가

---
+++ Components/Search/FilterOpenChat.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { AllOpenChat } from '@/app/search/search.type';
+
+const FilterOpenChat = ({ initialData }: { initialData: AllOpenChat }) => {
+	console.log(initialData);
+	return (
+		<>
+			<hr />
+			<h2>FilterOpenChat</h2>
+			<hr />
+		</>
+	);
+};
+
+export default FilterOpenChat;
