3ae2469e9f39bf503e4017a23bb822996f51ff6a
89882 <pgh123166@naver.com>
2023-11-09 11:17:42 +0900

    design: theme border radius 추가

--- theme.js
+++ theme.js
@@ -218,5 +218,18 @@ const theme = extendTheme({
     80: '20rem',
     96: '24rem',
   },
+  borderRadius: {
+    radii: {
+      none: '0',
+      sm: '0.125rem',
+      base: '0.25rem',
+      md: '0.375rem',
+      lg: '0.5rem',
+      xl: '0.75rem',
+      '2xl': '1rem',
+      '3xl': '1.5rem',
+      full: '9999px',
+    },
+  },
 });
 export default theme;
