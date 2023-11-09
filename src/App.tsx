dc9739cdfc3fabbcc932d90cddc8ba035d119735
Yonghun <yonghuni484@gmail.com>
2023-11-09 14:49:04 +0900

    Feat: Add game route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import AdminMain from "./pages/Main/user";
 import Login from "./pages/Login";
 import Example from "./pages/Example";
+import Game from "./pages/Game";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/main" element={<AdminMain />} />
+        <Route path="/game" element={<Game />} />
         <Route path="/example" element={<Example />} />
       </Routes>
     </>
