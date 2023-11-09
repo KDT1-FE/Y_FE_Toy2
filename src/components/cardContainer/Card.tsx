9df8bf005dc2f3a925f020c20e20e325f14f3f23
gaori <monooi@naver.com>
2023-11-09 23:01:24 +0900

    feat: 카드 컴포넌트 제작

---
+++ src/components/cardContainer/Card.tsx
@@ -0,0 +1,38 @@
+import {
+  Avatar,
+  AvatarGroup,
+  Badge,
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Text,
+} from '@chakra-ui/react';
+import { Chat } from './card.types';
+
+interface Props {
+  chat: Chat;
+}
+
+const UserCard = ({ chat }: Props) => {
+  return (
+    <Card border='2px solid #cdcdcd' boxShadow={0} w={215}>
+      <CardHeader>
+        <Heading as='h2' size='sm'>
+          {chat.name}
+        </Heading>
+        <Text fontSize='xs'>2023-11-08</Text>
+        <Badge colorScheme='yellow' px='2' mt='2'></Badge>
+      </CardHeader>
+      <CardBody>
+        <AvatarGroup size='sm' max={3}>
+          {chat.users.map((user) => (
+            <Avatar key={user.id} name={user.name} />
+          ))}
+        </AvatarGroup>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default UserCard;
