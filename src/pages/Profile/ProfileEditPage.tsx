d10a598fbd1a0e9b28d4e406423c8472d087e70e
YongYong21 <imkinggosu@gmail.com>
2023-11-10 11:14:25 +0900

    Fixed: 프로필 페이지 헤더 푸터 삭제

--- src/pages/Profile/ProfileEditPage.tsx
+++ src/pages/Profile/ProfileEditPage.tsx
@@ -1,13 +1,9 @@
 import ProfileEdit from "../../components/Profile/ProfileEdit";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
 
 function ProfileEditPage(): JSX.Element {
   return (
     <>
-      <Header></Header>
       <ProfileEdit></ProfileEdit>
-      <Footer></Footer>
     </>
   );
 }
--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -1,13 +1,9 @@
 import Profile from "../../components/Profile/Profile";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
 
 function ProfilePage(): JSX.Element {
   return (
     <>
-      <Header></Header>
       <Profile></Profile>
-      <Footer></Footer>
     </>
   );
 }
