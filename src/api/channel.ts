3b63e8c6514397118e6f849f71d96537d541be5d
gaori <monooi@naver.com>
2023-11-10 17:26:03 +0900

    fix: 채널 api headers 제거

--- src/api/channel.ts
+++ src/api/channel.ts
@@ -2,13 +2,7 @@ import { Channel } from '../@types/chat';
 import instance from './axios';
 
 export const getChannels = async () => {
-  const response = await instance.get<Channel[]>('/chat/all', {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.REACT_APP_USER_TOKEN}`,
-      serverId: process.env.REACT_APP_SERVER_ID,
-    },
-  });
+  const response = await instance.get<Channel[]>('/chat/all');
 
   return response.data;
 };
