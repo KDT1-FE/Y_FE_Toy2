8b0cde0964f93dd25927aa4f51788b604d5955f0
yangjaehyuk <gndksakxk10@naver.com>
2023-11-10 10:32:31 +0900

    Design: footer 디자인 완료

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import styled from 'styled-components';
 
 const Footer = () => {
-  return <div>footer</div>;
+  return <Title>© 2023 Toy Project Team 1, all rights reserved.</Title>;
 };
 
+const Title = styled.div`
+  line-height: 28px;
+  font-size: 12px;
+  font-weight: 400;
+  color: #a0aec0;
+  cursor: default;
+`;
 export default Footer;
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 import { titleAction } from '../../util/util';
 import { useNavigate } from 'react-router-dom';
