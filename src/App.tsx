8c7bea869d540ef28bcce8ca30105e10e035ab8b
IAMISTP <hyeminpak@gmail.com>
2023-11-10 17:46:22 +0900

    Feat : 게임방 목록페이지 route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import AdminMain from "./pages/Main/user";
 import Login from "./pages/Login";
 import Example from "./pages/Example";
 import Game from "./pages/Game";
+import GameLists from "./components/Main/GameLists";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/main" element={<AdminMain />} />
         <Route path="/game" element={<Game />} />
         <Route path="/example" element={<Example />} />
+        <Route path="/gameLists" element={<GameLists />} />
       </Routes>
     </>
   );
