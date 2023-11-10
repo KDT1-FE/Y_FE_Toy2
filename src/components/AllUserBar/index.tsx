3354ece576ab11b1c3993cfb45bc9db140e53961
zoeyourlife <201814112@office.skhu.ac.kr>
2023-11-10 10:48:08 +0900

    feat: 전체 유저 사이드 바 컴포넌트 구현

--- src/components/AllUserBar/index.tsx
+++ src/components/AllUserBar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Avatar,
   AvatarBadge,
@@ -8,35 +8,40 @@ import {
   Stack,
   Text,
 } from '@chakra-ui/react';
+import { useUserData } from '../../hooks/useUserData';
 
 const allUserBar = () => {
+  const userData = useUserData();
+
   return (
     <Box flex="1">
       <Flex>
         <Box flex="3"></Box>
-        <Stack direction="row" h="100vh" color="{white}">
-          <Divider mt={3} orientation="vertical" borderColor={'gray.400'} />
+        <Stack direction="row" h="100vh">
+          <Divider mt="12" orientation="vertical" borderColor={'gray.400'} />
         </Stack>
-        <Box flex="1" w="280px" h="100vh" bg="gray.50" p={30}>
-          <Text fontSize="1g" fontWeight="normal" color="gray.400" mt={10}>
-            전체ㅡ 숫자
+        <Box flex="1" w="280px" h="100vh" bg="gray.50" p="6">
+          <Text fontSize="1g" fontWeight="normal" color="gray.400" mt="8">
+            전체ㅡ {userData.totalUsers}
           </Text>
-          <Divider mt={3} borderColor={'gray.500'} />
-          <Flex mt={5}>
-            <Stack direction="column" spacing={4}>
-              <Avatar
-                size="sm"
-                name="Kent Dodds"
-                src="https://bit.ly/kent-c-dodds"
-              >
-                <AvatarBadge boxSize="1.25em" bg="green.500" />
-              </Avatar>
+          <Divider mt="4" borderColor={'gray.500'} />
+          <Flex mt="4" align="center">
+            <Stack direction="column" spacing="0">
+              {userData.userNames.map((userName, index) => (
+                <Flex key={index} mt="4">
+                  <Avatar
+                    size="sm"
+                    name={userName}
+                    src={userData.profilePictures[index]}
+                  >
+                    <AvatarBadge boxSize="1.25em" bg="green.500" />
+                  </Avatar>
+                  <Text fontSize="sm" color="black" ml="3" mt="1">
+                    {userName}
+                  </Text>
+                </Flex>
+              ))}
             </Stack>
-            <Flex align="center" justify="center">
-              <Text fontSize="sm" color="black" ml={3}>
-                새콤달콤
-              </Text>
-            </Flex>
           </Flex>
         </Box>
       </Flex>
