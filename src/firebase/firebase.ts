0a55d6a0340eff8fadd58e6b8dd45e20ee06a718
YongYong21 <imkinggosu@gmail.com>
2023-11-10 11:25:58 +0900

    Update firebase.ts

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
