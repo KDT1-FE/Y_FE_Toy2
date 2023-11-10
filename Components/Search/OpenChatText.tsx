235538a6b3c8e5b69767ca5937d86fcc4b0baa01
JitHoon <imessi0336@gmail.com>
2023-11-10 20:28:25 +0900

    Feat: 오픈 채팅방 목록 텍스트에 최근 대화 시간 추가

--- Components/Search/OpenChatText.tsx
+++ Components/Search/OpenChatText.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Chat } from '@/app/search/search.type';
+import { timeForToday } from '@/app/search/search.utils';
 
 const OpenChatText = ({ openChat }: { openChat: Chat }) => {
+	const fewMinutesAgo = timeForToday(openChat.updatedAt);
+
 	return (
 		<>
-			<h2>{openChat.name}</h2>
-			<span>{openChat.users.length}</span>
-			<span>{openChat.updatedAt.toString()}</span>
+			<div className="flex flex-col justify-center w-3/4 ml-2">
+				<h2 className="text-sm">{openChat.name}</h2>
+				<div className="w-full flex">
+					<span className="ml-0.5 text-sm">{openChat.users.length} 명</span>
+					<span className="ml-2 text-sm">{fewMinutesAgo.toString()}</span>
+				</div>
+			</div>
 		</>
 	);
 };
