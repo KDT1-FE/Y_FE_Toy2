2365e5fef354e68ff30b400d5a0bd2b6faaf738c
89882 <pgh123166@naver.com>
2023-11-10 13:58:19 +0900

    feat: Message 전송 기능 구현

--- src/components/chats/ChatInput.tsx
+++ src/components/chats/ChatInput.tsx
@@ -1,24 +1,44 @@
 import { Button } from '@chakra-ui/button';
 import { Input, InputGroup, InputRightElement } from '@chakra-ui/input';
+import socket from '../../api/socket';
+import { useState } from 'react';
+import { SOCKET } from '../../constants/socket';
 
 const ChatInput = () => {
+  const [value, setValue] = useState('');
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+  const sendChat = () => {
+    socket.emit(SOCKET.MESSAGE_TO_SERVER, value);
+    setValue('');
+  };
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      sendChat();
+    }
+  };
+
   return (
     <InputGroup size="lg" zIndex="1" maxWidth={700} m="0 auto">
       <Input
         pr="4.5rem"
         overflowY="scroll"
+        value={value}
         type="text"
         placeholder="Message to Channel"
         fontSize="md"
         bg="gray.300"
         color="blackAlpha.900"
         focusBorderColor="white"
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
         _focus={{
           bg: 'gray.300',
         }}
       />
       <InputRightElement width="4.5rem">
-        <Button h="1.75rem" size="sm">
+        <Button h="1.75rem" size="sm" onClick={sendChat}>
           Go
         </Button>
       </InputRightElement>
