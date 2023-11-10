fbd48b74692ff62e8ae489491c1b16aa26ce0445
jimin <moana16@naver.com>
2023-11-10 13:40:12 +0900

    fix : socket 연결 오류 해결

--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -1,38 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import { Message } from '@types/Message';
-import socket from '../../apis/socket';
+import React, { useState, useEffect, useRef } from 'react';
+import { io, Socket } from 'socket.io-client';
+import { Message } from '../../@types/types';
+import { CLIENT_URL } from '../../apis/constant';
 
 export default function Chat() {
+   // chatId랑 accessToken받아서 사용하기
   const [isConnected, setIsConnected] = useState(false);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState<string>('');
   const [messages, setMessages] = useState<Message[]>([]);
 
+  const socketRef = useRef<Socket | null>(null);
+
+  const chatId = '43c7d302-1005-4a55-b12c-3f6d30c4755c';
+
+  const accessToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNiN2ZiMTExZTp1c2VyMyIsImlhdCI6MTY5OTUzMzExMiwiZXhwIjoxNzAwMTM3OTEyfQ.4eslctzcBGQAwkcKT97IbF0i-9-MZ0kvhjY4A6sK8Wo';
+
   useEffect(() => {
-    socket.on('connect', () => {
-      console.log('Connected to the chat server');
+    socketRef.current = io(`${CLIENT_URL}?chatId=${chatId}`, {
+      extraHeaders: {
+        Authorization: `Bearer ${accessToken}`,
+        serverId: process.env.NEXT_PUBLIC_API_KEY,
+      },
+    });
+
+    socketRef.current.on('connect', () => {
+      console.log('Connected to chat server');
       setIsConnected(true);
     });
-
-    socket.on('disconnect', reason => {
-      console.log(`Disconnected: ${reason}`);
-      setIsConnected(false);
-    });
-
-    socket.on('messages-to-client', (messageObject: Message) => {
+    socketRef.current.on('message-to-client', (messageObject: Message) => {
+      console.log(messageObject);
       setMessages(prevMessages => [...prevMessages, messageObject]);
     });
 
     return () => {
-      socket.off('connect');
-      socket.off('disconnect');
-      socket.off('message-to-client');
+      socketRef.current?.off('connect');
+      socketRef.current?.off('message-to-client');
+      socketRef.current?.disconnect();
     };
-  }, []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // 이제 chatId와 accessToken이 변경될 때만 socket이 재생성됩니다.
 
   const handleSendMessage = (event: React.FormEvent) => {
     event.preventDefault();
-    socket.emit('message-to-server', { RequestData: message });
-    setMessage('');
+    console.log('message : ', message);
+    console.log(socketRef.current);
+     if (message && socketRef.current?.connected) {
+      socketRef.current.emit('message-to-server', message);
+      setMessage('');
+    }
   };
 
   return (
