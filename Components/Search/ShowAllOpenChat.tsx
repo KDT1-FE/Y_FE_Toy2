ee5194f4ff7245add75e9821b4c2d788ae05d34c
JitHoon <imessi0336@gmail.com>
2023-11-11 18:37:43 +0900

    Fix: 오픈 채팅 검색 페이지 디자인 및 props 추가

--- Components/Search/ShowAllOpenChat.tsx
+++ Components/Search/ShowAllOpenChat.tsx
@@ -5,16 +5,12 @@ import { Chat } from '@/app/search/search.type';
 import OpenChatText from './OpenChatText';
 import OpenChatPicture from './OpenChatPicture';
 
-const ShowAllOpenChat = ({ openChat }: { openChat: Chat }) => {
+const ShowAllOpenChat = ({ chat }: { chat: Chat }) => {
+	const TEXT_SIZE = 'text-sm';
 	return (
 		<>
-			<li
-				key={openChat.id}
-				className="w-full flex justify-between py-3 border-b-2 border-black cursor-pointer"
-			>
-				<OpenChatText openChat={openChat} />
-				<OpenChatPicture openChatUsers={openChat.users} />
-			</li>
+			<OpenChatText openChat={chat} textSize={TEXT_SIZE} />
+			<OpenChatPicture openChatUsers={chat.users} />
 		</>
 	);
 };
