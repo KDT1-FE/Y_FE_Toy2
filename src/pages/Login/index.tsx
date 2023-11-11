df2800e0c9f336296e0ee6d8f1ed14a8010e8ac5
suyeonnnnnnn <suyeon9923@naver.com>
2023-11-12 02:15:23 +0900

    Feat: Add login failure message

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,56 +1,76 @@
-import { Button, Input } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Button,
+  CloseButton,
+  Input,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import SignUpModal from "../../components/Login/SignUpModal";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
 import useFetch from "../../hooks/useFetch";
 import useInput from "../../hooks/useInput";
-
-interface Data {
-  id: string;
-  password: string;
-}
+import SignUpModal from "../../components/Login/SignUpModal/index";
 
 const Login = () => {
   const idInput = useInput("");
   const pwInput = useInput("");
-  const [data, setData] = useState<Data>({
-    id: "",
-    password: "",
-  });
-
+  const { setToken } = useAuth();
+  const navigate = useNavigate();
   const login = useFetch({
     url: "https://fastcampus-chat.net/login",
     method: "POST",
-    data: data,
+    data: { id: idInput.value, password: pwInput.value },
     start: false,
   });
-
-  useEffect(() => {
-    const copy = { ...data };
-    copy.id = idInput.value;
-    copy.password = pwInput.value;
-
-    setData(copy);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [idInput.value, pwInput.value]);
+  const [loginError, setLoginError] = useState("");
 
   const handleLogin = () => {
     login.refresh();
   };
 
   useEffect(() => {
-    if (login.result) {
-      const text = JSON.stringify({ ...login.result, id: idInput.value });
-      localStorage.setItem("token", text);
+    if (login.result && login.statusCode === 200) {
+      const { accessToken, refreshToken } = login.result;
+      if (accessToken && refreshToken) {
+        setToken(accessToken, refreshToken);
+        navigate("/main");
+      }
+    } else if (login.result && login.statusCode !== 200) {
+      setLoginError("로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요.");
     }
-    console.log(login.result, login.loading, login.statusCode);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [login.result]);
+  }, [login.result, login.statusCode, setToken, navigate]);
 
   return (
     <>
       <div>로그인</div>
-      <Input type="text" value={idInput.value} onChange={idInput.onChange} />
-      <Input type="text" value={pwInput.value} onChange={pwInput.onChange} />
+      {loginError && (
+        <Alert status="error">
+          <AlertIcon />
+          <AlertTitle mr={2}>로그인 실패</AlertTitle>
+          <AlertDescription>{loginError}</AlertDescription>
+          <CloseButton
+            position="absolute"
+            right="8px"
+            top="8px"
+            onClick={() => setLoginError("")}
+          />
+        </Alert>
+      )}
+      <Input
+        type="text"
+        placeholder="아이디"
+        value={idInput.value}
+        onChange={idInput.onChange}
+      />
+      <Input
+        type="password"
+        placeholder="비밀번호"
+        value={pwInput.value}
+        onChange={pwInput.onChange}
+      />
       <Button onClick={handleLogin}>로그인</Button>
       <SignUpModal />
     </>
