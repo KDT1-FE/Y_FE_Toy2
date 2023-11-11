4226a4dd6bd49880f6bd942cbb55dfe8c1dad795
furaha707 <zxcvyein950823@gmail.com>
2023-11-12 03:15:20 +0900

    Fix: 채팅방 리스트 출력

--- src/Pages/Chat.tsx
+++ src/Pages/Chat.tsx
@@ -17,7 +17,7 @@ export interface ChatI {
   users: User[];
   isPrivate: boolean;
   updatedAt: string;
-  latestMessage: string | null;
+  latestMessage: string;
 }
 
 function Chat() {
@@ -25,74 +25,49 @@ function Chat() {
   const { getData } = useApi();
   const { accessToken } = useContext(AuthContext);
 
-  // const fetchMyRoom = async () => {
-  //   try {
-  //     const response = await axios.get('https://fastcampus-chat.net/chat', {
-  //       headers: {
-  //         'Authorization': `Bearer ${token}`,
-  //         serverId: '1601075b',
-  //       }
-  //     });
-  
-  //     const roomData = response.data
-  //     return roomData;
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  // 모든 채팅방 조회
+  // 나의 채팅방 조회
   useEffect(() => {
     const fetchData = async () => {
       try{
         const data = await getData("https://fastcampus-chat.net/chat");
         const chatData = data.chats;
         console.log(chatData);
+
+        const myRoom = chatData.map((room: ChatI) => {
+
+          // 시간 계산
+          const updatedAt = room.updatedAt;
+          const givenDate: Date = new Date(updatedAt);
+          const currentDate: Date = new Date();
+          const timeDifference = currentDate.getTime() - givenDate.getTime();
+          const minutesDifference = Math.floor(timeDifference / (1000 * 60));
+
+          let updatedAtString: string;
+
+          if (minutesDifference < 1) {
+            updatedAtString = "방금 전";
+          } else if (minutesDifference < 60) {
+            updatedAtString = `${minutesDifference}분 전`;
+          } else {
+            const hoursDifference = Math.floor(minutesDifference / 60);
+            updatedAtString = `${hoursDifference}시간 전`;
+          }
+
+          // const latestMessage = room.latestMessage || "메시지가 없습니다.";
+          return {
+            ...room,
+            updatedAt: updatedAtString,
+            // latestMessage: latestMessage
+          };
+        });
+
+        setChatRoom(myRoom);
       } catch (error){
         console.error(error)
       }
     }
     fetchData();
-    // const fetchData = async () => {
-    //   try {
-    //     const data = await fetchMyRoom();
-    //     const myRoom = data.chats.map((room: ChatI) => {
-    //       // 시간 계산
-    //       const updatedAt = room.updatedAt;
-    //       const givenDate: Date = new Date(updatedAt);
-    //       const currentDate: Date = new Date();
-    //       const timeDifference = currentDate.getTime() - givenDate.getTime();
-    //       const minutesDifference = Math.floor(timeDifference / (1000 * 60));
-
-    //       let updatedAtString: string;
-
-    //       if (minutesDifference < 1) {
-    //         updatedAtString = "방금 전";
-    //       } else if (minutesDifference < 60) {
-    //         updatedAtString = `${minutesDifference}분 전`;
-    //       } else {
-    //         const hoursDifference = Math.floor(minutesDifference / 60);
-    //         updatedAtString = `${hoursDifference}시간 전`;
-    //       }
-
-    //       const latestMessage = room.latestMessage || "메시지가 없습니다.";
-
-    //       return {
-    //         ...room,
-    //         updatedAt: updatedAtString,
-    //         latestMessage: latestMessage
-    //       };
-    //     });
-
-    //     setChatRoom(myRoom);
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
-    // };
-
-    // fetchData();
   }, [accessToken]);
-  
 
   return (
     <>
@@ -101,12 +76,12 @@ function Chat() {
       </div> */}
       <ChatWrapper>
         <ChatCategory>
-          {chatRoom.map((room) => (
-            <CateLink>
-              <div className="catelink__wrap">
+          { chatRoom.map((room) => (
+            <CateLink key={room.id}>
+             <div className="catelink__wrap">
                 <div className="catelink__name">
                   <p className="tit">{room.name}</p>
-                  <p className="content">{room.latestMessage}</p>
+                  {/* <p className="content">{room.latestMessage}</p> */}
                 </div>
                 <div className="catelink__time">{room.updatedAt}</div>
               </div>
@@ -140,52 +115,22 @@ const ChatCategory = styled.ul`
 `;
 
 const CateLink = styled.li`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
   padding: 10px;
   border-bottom: 1px solid #e8e8e8;
   .catelink {
     &__wrap {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
       gap: 16px;
       width: 100%;
     }
-    &__img {
-      width: 70px;
-      border-radius: 50%;
-      display: block;
-      position: relative;
-      overflow: hidden;
-      img {
-        display: block;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        width: auto;
-        height: auto;
-        min-width: 1000%;
-        min-height: 1000%;
-        max-width: none;
-        max-height: none;
-        -webkit-transform: translate(-50%, -50%) scale(0.1);
-        transform: translate(-50%, -50%) scale(0.1);
-      }
-      &:after {
-        content: "";
-        display: block;
-        padding-bottom: 100%;
-      }
-    }
     &__name {
       font-size: 16px;
-      flex: 1 0 50%;
-      max-width: 50%;
       .tit {
         margin-bottom: 10px;
         color: black;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        padding-bottom: 2px;
       }
       .content {
         white-space: nowrap;
@@ -195,8 +140,7 @@ const CateLink = styled.li`
       }
     }
     &__time {
-      flex: 1 0 15%;
-      max-width: 15%;
+      text-align:right;
       font-size: 14px;
       color: #999696;
       white-space: nowrap;
