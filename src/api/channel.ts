d581b37bfc828e2a544fce34f0634d956c8a33cc
gaori <monooi@naver.com>
2023-11-11 19:58:24 +0900

    feat: 채널 생성 api 추가

--- src/api/channel.ts
+++ src/api/channel.ts
@@ -1,8 +1,27 @@
 import { ResponseValue } from '../@types/channel';
 import instance from './axios';
 
+export interface CreateChannelBody {
+  name: string;
+  users: string[];
+  isPrivate: boolean;
+}
+
 export const getChannels = async () => {
   const response = await instance.get<ResponseValue>('/chat/all');
 
   return response.data.chats;
+};
+
+export const createChannel = async (data: CreateChannelBody) => {
+  try {
+    const response = await instance.post('/chat', {
+      name: data.name,
+      users: data.users,
+      isPrivate: data.isPrivate,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
