fa2dc6a0aa5bf7ce66fbf50600705f2bd4f40390
gkdus2217 <gkdus2217@gmail.com>
2023-11-12 21:56:01 +0900

    Feat : 로비 페이지 네이션 기능

--- src/components/layout/checkGameRoom.tsx
+++ src/components/layout/checkGameRoom.tsx
@@ -1,50 +1,145 @@
+// checkGameRoom.tsx
+
+import { useState, useEffect } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { accessTokenState, allRoomState } from '../../states/atom';
 import { getAllGameRooms, participateGameRoom } from '../../api';
 import { useNavigate } from 'react-router-dom';
 import usePollingData from '../template/usePollingData';
+import Pagination from 'react-js-pagination';
+import {
+  Center,
+  Flex,
+  FormControl,
+  FormLabel,
+  Link,
+  Input,
+  Button,
+  Img,
+  Switch,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  CloseButton,
+  Box,
+  Fade,
+  ListItem,
+  UnorderedList,
+  List,
+} from '@chakra-ui/react';
+import styled from 'styled-components';
 
 const CheckGameRoom = () => {
   const navigate = useNavigate();
   const [allRooms, setAllRooms] = useRecoilState(allRoomState);
   const accessToken: any = useRecoilValue(accessTokenState);
 
+  const [showAlert, setShowAlert] = useState({
+    active: false,
+    message: '',
+    type: '',
+  });
+
+  // Pagination 관련 상태와 핸들러 추가
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalItemsCount, setTotalItemsCount] = useState(0);
+  const itemsPerPage = 10; // 한 페이지당 표시할 아이템 수
+
+  const handlePageChange = (page: any) => {
+    setCurrentPage(page);
+  };
+
   const fetchData = async () => {
     try {
       const allRoomsData = await getAllGameRooms(accessToken);
-      if (JSON.stringify(allRoomsData.chats) !== JSON.stringify(allRooms)) {
-        setAllRooms(allRoomsData.chats);
-      }
+      setTotalItemsCount(allRoomsData.chats.length);
+
+      // 방번호 넣기
+      const plusIndex = {
+        ...allRoomsData,
+        chats: allRoomsData.chats.map((room, index) => ({
+          ...room,
+          index: index + 1,
+        })),
+      };
+
+      // 서버에서 받아온 전체 데이터를 현재 페이지에 맞게 자름
+      const startIndex = (currentPage - 1) * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+      const paginatedRooms = plusIndex.chats.slice(startIndex, endIndex);
+
+      setAllRooms(paginatedRooms);
+      console.log(paginatedRooms);
     } catch (error) {
       console.error('Error retrieving data:', error);
     }
   };
 
-  usePollingData(fetchData, [allRooms, setAllRooms]);
+  usePollingData(fetchData, [accessToken, currentPage]);
 
   const handleParticipate = async (numberOfPeople: number, chatId: any) => {
     if (numberOfPeople === 4) {
-      alert('방이 꽉 찼어요.');
+      const errorMessage = `방이 꽉 찼어요.`;
+      const errorType = 'full';
+      setShowAlert({ active: true, message: errorMessage, type: errorType });
+      console.log(showAlert);
     } else {
       await participateGameRoom(chatId, accessToken);
       navigate(`/room/:${chatId}`);
     }
   };
 
+  useEffect(() => {
+    // alert 창 3초 후에 사라지게 하기
+    if (showAlert.active) {
+      const timer = setTimeout(() => {
+        setShowAlert({ active: false, message: '', type: '' });
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [showAlert.active]);
+
   return (
     <>
-      <button onClick={fetchData}>Refresh Data</button>
-      {allRooms.map((element, index) => (
-        <div
-          key={index}
-          onClick={() => handleParticipate(element.users.length, element.id)}>
-          <p>{element.name}</p>
-          <p>{element.id}</p>
-          <p>{element.users.length}</p>
-          {element.users.length === 4 && <p>Four users in this room</p>}
-          <p>{element.users[0].id}</p>
-        </div>
-      ))}
+      <List spacing={3}>
+        {allRooms.map((element, index) => (
+          <ListItem
+            key={index}
+            onClick={() => handleParticipate(element.users.length, element.id)}>
+            <p>{element.index}</p>
+            <p>{element.name}</p>
+            <p>{element.users.length} / 4</p>
+          </ListItem>
+        ))}
+
+        {/* Pagination 컴포넌트를 추가합니다. */}
+        <Pagination
+          activePage={currentPage}
+          itemsCountPerPage={itemsPerPage}
+          totalItemsCount={totalItemsCount}
+          pageRangeDisplayed={5}
+          prevPageText={'‹'}
+          nextPageText={'›'}
+          onChange={handlePageChange}
+        />
+      </List>
+
+      <Fade in={showAlert.active}>
+        <Alert
+          marginTop={10}
+          status="error"
+          width={400}
+          height={70}
+          variant="solid"
+          borderRadius={6}>
+          <AlertIcon />
+          <Box>
+            <AlertTitle mr={2}>방 입장 오류</AlertTitle>
+            <AlertDescription>{showAlert.message}</AlertDescription>
+          </Box>
+        </Alert>
+      </Fade>
     </>
   );
 };
--- src/pages/lobby/gameLobby.tsx
+++ src/pages/lobby/gameLobby.tsx
@@ -1,57 +1,57 @@
 import { useState } from 'react';
 import { getUsers } from '../../api/index';
-import CreateGameRoom from '../../components/layout/createGameRoom';
-import CheckGameRoom from '../../components/layout/checkGameRoom';
-import { Button } from '@chakra-ui/react';
-import { handleGetAllUsers } from '../../util/util';
-import { handlePostRefresh } from '../../util/util';
 import { accessTokenState } from '../../states/atom';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
-import OfflineUserList from '../../components/layout/offlineUserList';
 import OnlineUserList from '../../components/layout/onlineUserList';
-import UserList from '../../components/layout/userList';
-import UserLogout from '../login/userLogout';
-import CheckPrivateChat from '../../components/layout/checkPrivateChat';
 import MyUserData from '../../components/layout/MyUserData';
+import {
+  Center,
+  Flex,
+  FormControl,
+  FormLabel,
+  Link,
+  Input,
+  Button,
+  Img,
+  Switch,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  CloseButton,
+  Box,
+  Fade,
+} from '@chakra-ui/react';
+import styled from 'styled-components';
+import CheckGameRoom from '../../components/layout/checkGameRoom';
 
 const GameLobby = () => {
-  const setAccessToken = useSetRecoilState(accessTokenState); // hook 규칙으로 함수 외부에있어야함
-
-  const accessToken = useRecoilValue(accessTokenState);
-  const [imgsrc, setImgsrc] = useState('');
-
-  async function fetchUserData() {
-    try {
-      const res = await getUsers(accessToken, 'hojin');
-      const user = res;
-      setImgsrc(user.data[4].picture);
-    } catch (error) {
-      console.error(error);
-      // 에러 처리 로직
-    }
-  }
-
   return (
     <>
-      <Button onClick={fetchUserData}>이미지나와랏</Button>
-      <Button onClick={handlePostRefresh(setAccessToken)}>토큰재발급</Button>
-      <Button onClick={handleGetAllUsers(accessToken)}>모든유저보기</Button>
-      <h2 style={{ fontWeight: 'bold' }}>1:1대화</h2>
-      <CheckPrivateChat></CheckPrivateChat>
-      <UserList></UserList>
-      <MyUserData></MyUserData>
-      <br></br>
-      <OnlineUserList />
-      <br></br>
-      <OfflineUserList />
-      <br />
-      <CreateGameRoom></CreateGameRoom>
-      <br></br>
-      <CheckGameRoom></CheckGameRoom>
-      <br></br>
-      <UserLogout />
+      <Flex
+        justifyContent={'space-between'}
+        alignItems={'center'}
+        flexDirection={'row'}
+        height={700}
+        width={1400}>
+        <LeftComponent>
+          <CheckGameRoom />
+        </LeftComponent>
+        <RightComponent>
+          <OnlineUserList />
+          <MyUserData />
+        </RightComponent>
+      </Flex>
     </>
   );
 };
+
+const LeftComponent = styled.div`
+  width: 930px;
+`;
+
+const RightComponent = styled.div`
+  width: 450px;
+`;
 
 export default GameLobby;
