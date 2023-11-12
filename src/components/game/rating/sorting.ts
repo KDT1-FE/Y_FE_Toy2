e308942ea13ba7833b7160d502a59fa0235320b0
Yamyam-code <rnffjt@gmail.com>
2023-11-13 02:54:14 +0900

    Feat: 랭킹 firebase 연결

--- src/components/game/rating/sorting.ts
+++ src/components/game/rating/sorting.ts
@@ -1,8 +1,9 @@
 export interface Id {
-  id: string;
+  name: string;
   correct: number;
 }
 
-const sorting = (idArray: Id[]) => idArray.sort((a, b) => b.correct - a.correct || a.id.localeCompare(b.id));
+const sorting = (idArray: Id[]) =>
+  idArray.sort((a, b) => b.correct - a.correct || a.name.localeCompare(b.name));
 
 export default sorting;
