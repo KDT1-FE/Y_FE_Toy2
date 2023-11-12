9b56909b6d51760973b384ec8b219689049a1bc0
jiohjung98 <ckaquz98@naver.com>
2023-11-12 18:30:59 +0900

    feat: 내채팅페이지에서 채팅버튼 클릭 시 1:1 채팅 기능 구현

--- src/app/[userId]/page.tsx
+++ src/app/[userId]/page.tsx
@@ -4,8 +4,11 @@ import React, { useState } from 'react';
 import ChatPage from '@/components/chats/ChatPage';
 import Navigation from '@/components/Navigation';
 
+
+
 const MyChats = () => {
     const userId = sessionStorage.getItem('userId');
+    
     return (
         <>
             <ChatPage userType="my" />
