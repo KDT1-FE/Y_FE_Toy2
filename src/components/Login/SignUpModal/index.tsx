3b7f62cebfbbdb27ee8ecef965d137cf681162a8
sue <dldustn2040@gmail.com>
2023-11-13 17:37:25 +0900

    Revert "Merge branch 'develop' into feature/#3"
    This reverts commit 8f8f8b34cf0959da3cf93ac337970fdb44d4c7a9, reversing
    changes made to d0431c0f12a190253b4f7a602d23c55ec12131e1.

--- src/components/Login/SignUpModal/index.tsx
+++ src/components/Login/SignUpModal/index.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import { useForm, Controller } from "react-hook-form";
-import { Input, Button, Text, Alert, AlertIcon } from "@chakra-ui/react";
-import useFetch from "../../../hooks/useFetch";
+import useInput from "../../../hooks/useInput";
+import { Input } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
-interface FormData {
+interface Data {
   id: string;
   password: string;
   name: string;
@@ -12,180 +12,46 @@ interface FormData {
 }
 
 const SignUpModal = () => {
-  const {
-    control,
-    handleSubmit,
-    setError,
-    clearErrors,
-    formState: { errors },
-  } = useForm<FormData>();
-  const signUpFetch = useFetch({
-    url: "https://fastcampus-chat.net/signup",
-    method: "POST",
-    data: {},
-    start: false,
+  const idInput = useInput("");
+  const pwInput = useInput("");
+  const nameInput = useInput("");
+  const [data, setData] = useState<Data>({
+    id: "",
+    password: "",
+    name: "",
   });
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  const [signUpStatus, setSignUpStatus] = useState<{
-    type: "success" | "error";
-    message: string;
-  } | null>(null);
 
-  const checkIdDuplication = async (id: string): Promise<boolean> => {
-    try {
-      const response = await axios.post(
-        "https://fastcampus-chat.net/check/id",
-        { id },
-      );
-      return response.data.isDuplicated;
-    } catch (error) {
-      console.error("Error checking ID duplication", error);
-      return false;
-    }
-  };
+  useEffect(() => {
+    const copy = { ...data };
+    copy.id = idInput.value;
+    copy.password = pwInput.value;
+    copy.name = nameInput.value;
 
-  // 회원가입 제출
-  const onSubmit = async (formData: FormData) => {
-    let pictureAsString: string | undefined;
+    setData(copy);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [idInput.value, pwInput.value, nameInput.value]);
 
-    // 파일을 Base64 문자열로 변환
-    const toBase64 = (file: File) =>
-      new Promise<string>((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => resolve(reader.result as string);
-        reader.onerror = (error) => reject(error);
-      });
-
-    if (selectedFile) {
-      pictureAsString = await toBase64(selectedFile);
-    }
-
-    const dataToSend = {
-      ...formData,
-      picture: pictureAsString,
-    };
-    // 회원가입 요청 및 결과 처리
-    try {
-      const response = await axios.post(
-        "https://fastcampus-chat.net/signup",
-        dataToSend,
-        {
-          headers: {
-            "content-type": "application/json",
-            serverId: import.meta.env.VITE_APP_SERVER_ID,
-          },
+  const handleSignup = () => {
+    axios
+      .post("https://fastcampus-chat.net/signup", data, {
+        headers: {
+          "content-type": "application/json",
+          serverId: "6603aca7",
         },
-      );
-      if (response.status === 200) {
-        console.log("회원가입 성공:", response.data);
-        setSignUpStatus({
-          type: "success",
-          message: "회원가입에 성공하였습니다.",
-        });
-      }
-    } catch (error) {
-      console.error("회원가입 실패:", error);
-      setSignUpStatus({ type: "error", message: "회원가입에 실패하였습니다." });
-    }
+      })
+      .then((res) => console.log(res));
   };
-
   return (
     <>
       <div>회원가입</div>
-      {signUpStatus && (
-        <Alert status={signUpStatus.type}>
-          <AlertIcon />
-          {signUpStatus.message}
-        </Alert>
-      )}
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          name="picture"
-          control={control}
-          render={({ field: { onChange } }) => (
-            <Input
-              type="file"
-              accept="image/*"
-              onChange={(event) => {
-                const file = event.target.files?.[0];
-                if (file) {
-                  if (file.size > 1024 * 1024) {
-                    console.error("File size exceeds 1MB");
-                  } else {
-                    setSelectedFile(file);
-                    onChange(file);
-                  }
-                }
-              }}
-            />
-          )}
-        />
-        <Controller
-          name="id"
-          control={control}
-          rules={{
-            required: "ID는 필수입니다.",
-            pattern: {
-              value: /^[A-Za-z0-9]+$/,
-              message: "ID는 영문자와 숫자만 사용할 수 있습니다.",
-            },
-            validate: async (id) => {
-              try {
-                const isDuplicated = await checkIdDuplication(id);
-                if (isDuplicated) {
-                  setError("id", {
-                    type: "manual",
-                    message: "이미 사용중인 ID입니다.",
-                  });
-                  return false;
-                }
-                clearErrors("id");
-                return true;
-              } catch (error) {
-                console.error("ID 중복 확인 중 오류 발생:", error);
-                return "ID 중복 확인 중 오류가 발생했습니다.";
-              }
-            },
-          }}
-          render={({ field }) => (
-            <Input type="text" placeholder="ID" {...field} />
-          )}
-        />
-        {errors.id && <Text color="red.500">{errors.id.message}</Text>}
-
-        <Controller
-          name="password"
-          control={control}
-          rules={{
-            required: "비밀번호는 필수입니다.",
-            minLength: {
-              value: 5,
-              message: "비밀번호는 5자리 이상이어야 합니다.",
-            },
-          }}
-          render={({ field }) => (
-            <Input type="password" placeholder="Password" {...field} />
-          )}
-        />
-        {errors.password && (
-          <Text color="red.500">{errors.password.message}</Text>
-        )}
-
-        <Controller
-          name="name"
-          control={control}
-          rules={{ required: "이름은 필수입니다." }}
-          render={({ field }) => (
-            <Input type="text" placeholder="Name" {...field} />
-          )}
-        />
-        {errors.name && <Text color="red.500">{errors.name.message}</Text>}
-
-        <Button type="submit" isLoading={signUpFetch.loading}>
-          가입
-        </Button>
-      </form>
+      <Input type="text" value={idInput.value} onChange={idInput.onChange} />
+      <Input type="text" value={pwInput.value} onChange={pwInput.onChange} />
+      <Input
+        type="text"
+        value={nameInput.value}
+        onChange={nameInput.onChange}
+      />
+      <Button onClick={handleSignup}>가입</Button>
     </>
   );
 };
--- src/pages/Example/index.tsx
+++ src/pages/Example/index.tsx
@@ -1,17 +1,13 @@
 import { Button, Input } from "@chakra-ui/react";
 import { serverTimestamp } from "firebase/firestore";
 import { useEffect, useState } from "react";
+// import { io } from "socket.io-client";
 import CreateGameModal from "../../components/Main/CreateGameModal";
 import ToastNotice from "../../components/common/ToastNotice";
 import useFetch from "../../hooks/useFetch";
 import useFireFetch from "../../hooks/useFireFetch";
 import useInput from "../../hooks/useInput";
-import connect from "../../socket/socket";
-
-interface MessageInfo {
-  id: string;
-  text: string;
-}
+import useSocket from "../../hooks/useSocket";
 
 const Example = () => {
   const token = JSON.parse(localStorage.getItem("token") as string);
@@ -27,10 +23,9 @@ const Example = () => {
   });
 
   // 소켓 통신
-  const socket = connect("9fe8a1af-9c60-4937-82dd-21d6da5b9cd9");
-
-  useEffect(() => {
-    socket.on("message-to-client", (messageObject) => {
+  const sendMessage = useSocket(
+    "9fe8a1af-9c60-4937-82dd-21d6da5b9cd9",
+    (messageObject) => {
       // 일반 채팅인지 초대 메시지인지 구별
       if (messageObject.text.slice(-5, -2) === "*&^") {
         // 초대 상태 저장
@@ -44,34 +39,30 @@ const Example = () => {
         setRoomData(room);
       } else {
         // 메시지 데이터, 작성 유저 상태 저장
-        const message = {
-          id: messageObject.userId,
-          text: messageObject.text,
-        };
-
-        console.log(message);
-        setMessages((prev) => [...prev, message]);
+        const copy = { ...message };
+        copy.id = messageObject.userId;
+        copy.text = messageObject.text;
+        setMessage(copy);
       }
-    });
+    },
+  );
 
-    // 채팅 기록 확인
-    socket.on("messages-to-client", (messageObject) => {
-      console.log(messageObject);
-    });
-
-    // 초대 메시지
-    socket.on("new-chat", (messageObject) => {
-      console.log(messageObject);
-    });
-
-    return () => {
-      socket.off("message-to-client");
-    };
-  }, [socket]);
+  // // 채팅 서버 연결
+  // const socket = io(
+  //   `https://fastcampus-chat.net/chat?chatId=9fe8a1af-9c60-4937-82dd-21d6da5b9cd9`,
+  //   {
+  //     extraHeaders: {
+  //       Authorization: `Bearer ${token.accessToken}`,
+  //       serverId: import.meta.env.VITE_APP_SERVER_ID,
+  //     },
+  //   },
+  // );
 
   // 메세지 데이터
-  const [messages, setMessages] = useState<MessageInfo[]>([]);
-
+  const [message, setMessage] = useState({
+    id: "",
+    text: "",
+  });
   // 초대방 정보 데이터
   const [roomData, setRoomData] = useState({
     id: "",
@@ -105,7 +96,7 @@ const Example = () => {
     url: "https://fastcampus-chat.net/chat/leave",
     method: "PATCH",
     data: {
-      chatId: "535add19-c98f-4a9b-bc6f-c145c496cb91",
+      chatId: "e6d8fd5b-00e3-4598-b826-11366c8c4676",
     },
     start: false,
   });
@@ -113,10 +104,40 @@ const Example = () => {
   // 메시지 input value 저장
   const messageValue = useInput("");
 
+  // // 소켓 통신 시 메시지 데이터 저장
+  // useEffect(() => {
+  //   socket.on("message-to-client", (messageObject) => {
+  //     // 일반 채팅인지 초대 메시지인지 구별
+
+  //     if (messageObject.text.slice(-5, -2) === "*&^") {
+  //       // 초대 상태 저장
+  //       const usersArr = JSON.parse(messageObject.text);
+  //       const users = [...usersArr];
+  //       users.pop();
+  //       users.pop();
+  //       const room = usersArr[usersArr.length - 2];
+
+  //       setToastUser(users);
+  //       setRoomData(room);
+  //     } else {
+  //       // 메시지 데이터, 작성 유저 상태 저장
+  //       const copy = { ...message };
+  //       copy.id = messageObject.userId;
+  //       copy.text = messageObject.text;
+  //       setMessage(copy);
+  //     }
+
+  //     // console.log(messageObject);
+  //   });
+
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [socket]);
+
   // 메시지 값 변화시(소켓 통신 시) 콘솔에 메시지 데이터 출력
   useEffect(() => {
-    console.log(messages);
-  }, [messages]);
+    if (message.id !== "") console.log(message);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [message.text]);
 
   //팝업 변화 감지
   useEffect(() => {
@@ -168,9 +189,7 @@ const Example = () => {
       body: "updated",
       createdAt: serverTimestamp(),
     };
-
     fireFetch.updateData("notice", "asdasdasdasdasd", newData);
-
     const copy = [...notice.data];
     const index = copy.findIndex((v) => v.id === "asdasdasdasdasd");
     copy[index] = newData;
@@ -178,22 +197,22 @@ const Example = () => {
     notice.setData(copy);
   };
 
-  const getMessages = () => {
-    socket.emit("fetch-messages");
+  const postData_A = () => {
+    console.log(1);
   };
 
-  const liveChat = () => {
+  const deleteData_A = () => {
     live.refresh();
   };
 
   // api get 요청으로 가져온 데이터 출력
-  const getFetchData = () => {
+  const getData_A = () => {
     console.log(users.result, users.loading, users.statusCode);
   };
 
   // 메시지 보내는 함수
   const submitMessage = () => {
-    socket.emit("message-to-server", messageValue.value);
+    sendMessage(messageValue.value);
   };
 
   return (
@@ -210,9 +229,9 @@ const Example = () => {
 
       {/* 벡엔드 rest api 통신 */}
       <div style={{ marginBottom: "2rem" }}>
-        <Button onClick={getMessages}>채팅 메시지 기록 확인</Button>
-        <Button onClick={liveChat}>나가기</Button>
-        <Button onClick={getFetchData}>Get</Button>
+        <Button onClick={postData_A}>Post</Button>
+        <Button onClick={deleteData_A}>나가기</Button>
+        <Button onClick={getData_A}>Get</Button>
       </div>
 
       {/* 메시지 소켓 통신 */}
