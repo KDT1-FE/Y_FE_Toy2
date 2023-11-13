04a41bd7c2fe3d11b3370a451fd819dab1dae04d
sunpaaaa <sunpaaaa@gmail.com>
2023-11-13 15:52:02 +0900

    📄Docs: change file structure

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@api': path.resolve(__dirname, './src/api'),
       '@styles': path.resolve(__dirname, './src/styles'),
       '@assets': path.resolve(__dirname, './src/assets'),
       '@components': path.resolve(__dirname, './src/components'),
@@ -14,5 +15,4 @@ export default defineConfig({
       '@store': path.resolve(__dirname, './src/store'),
     },
   },
-  plugins: [react()],
 });
