207943c513533fc219596191d6713c6eca7d0606
zoeyourlife <201814112@office.skhu.ac.kr>
2023-11-14 04:38:41 +0900

    test: 테스트 추가를 위한 카테고리 목록 추가

--- src/constants/channel.ts
+++ src/constants/channel.ts
@@ -1,6 +1,7 @@
 export const ALL_CHANNELS = ['channels'];
 
 export const CATEGORIES = [
+  '전체',
   '기타',
   '일상',
   '프로그래밍',
@@ -9,9 +10,11 @@ export const CATEGORIES = [
   '프론트엔드',
   '백엔드',
   '알고리즘',
+  '산책',
 ];
 
 export const CATEGORY_COLOR_SCHEMES: { [key: string]: string } = {
+  전체: 'skyblue',
   기타: 'red',
   일상: 'orange',
   프로그래밍: 'yellow',
@@ -20,4 +23,5 @@ export const CATEGORY_COLOR_SCHEMES: { [key: string]: string } = {
   프론트엔드: 'indigo',
   백엔드: 'purple',
   알고리즘: 'pink',
+  산책: 'gray',
 };
