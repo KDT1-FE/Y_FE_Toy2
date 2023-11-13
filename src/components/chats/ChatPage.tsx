1f16103d993da69f3fb7491635340a0e66355f47
jinjongsu <jjs9836@naver.com>
2023-11-13 22:37:23 +0900

    Fix: refetchInterval 추가

--- src/components/chats/ChatPage.tsx
+++ src/components/chats/ChatPage.tsx
@@ -17,113 +17,113 @@ import { sortTime } from './useFormatCreatedAt';
 import { getMyChats, getAllChats } from './getChats';
 import { useQuery } from '@tanstack/react-query';
 const MyChats = ({ userType }: any) => {
-    const [addChatOpen, setAddChatOpen] = useState(false);
-    const filterChats = useRecoilValue(searchChatsState);
-    const router = useRouter();
-    const userId = typeof window !== 'undefined' ? sessionStorage.getItem('userId') : null;
+  const [addChatOpen, setAddChatOpen] = useState(false);
+  const filterChats = useRecoilValue(searchChatsState);
+  const router = useRouter();
+  const userId = typeof window !== 'undefined' ? sessionStorage.getItem('userId') : null;
 
-    const enterChatRoom = (chat: Chat) => {
-        if (chat.id && chat.users) {
-            router.push(`/chating/${chat.id}`);
-        }
-    };
-    const { data, isLoading } = useQuery<Chat[], any>({
-        queryKey: ['getChatsKey'],
-        queryFn: userType === 'my' ? getMyChats : getAllChats,
-        refetchOnWindowFocus: false,
-        staleTime: 10000,
-    });
+  const enterChatRoom = (chat: Chat) => {
+    if (chat.id && chat.users) {
+      router.push(`/chating/${chat.id}`);
+    }
+  };
+  const { data } = useQuery<Chat[], unknown>({
+    queryKey: ['getChatsKey'],
+    queryFn: userType === 'my' ? getMyChats : getAllChats,
+    refetchOnWindowFocus: false,
+    staleTime: 10000,
+  });
 
-    const onAddHandler = () => {
-        setAddChatOpen(!addChatOpen);
-    };
+  const onAddHandler = () => {
+    setAddChatOpen(!addChatOpen);
+  };
 
-    return (
-        <Wrapper>
-            <ChatHeader>
-                <MyChatBar>{userType === 'all' ? '오픈 채팅' : '내 채팅'}</MyChatBar>
-                <IconBar>
-                    <AddChatIcon onClick={onAddHandler} />
-                </IconBar>
-            </ChatHeader>
-            <ChatContainer>
-                <SearchMyChat userType={userType} />
-                <ChatList>
-                    {userId && data
-                        ? filterChats.length > 0
-                            ? sortTime(filterChats).map((chat) => (
-                                  <MyChatItem
-                                      key={chat.id}
-                                      name={chat.name}
-                                      latestMessage={chat.latestMessage}
-                                      users={chat.users}
-                                      onClick={() => enterChatRoom(chat)}
-                                      isPrivate={chat.isPrivate}
-                                  />
-                              ))
-                            : sortTime(data).map((chat) => (
-                                  <MyChatItem
-                                      key={chat.id}
-                                      name={chat.name}
-                                      latestMessage={chat.latestMessage}
-                                      users={chat.users}
-                                      onClick={() => enterChatRoom(chat)}
-                                      isPrivate={chat.isPrivate}
-                                  />
-                              ))
-                        : null}
-                </ChatList>
-            </ChatContainer>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <ChatHeader>
+        <MyChatBar>{userType === 'all' ? '오픈 채팅' : '내 채팅'}</MyChatBar>
+        <IconBar>
+          <AddChatIcon onClick={onAddHandler} />
+        </IconBar>
+      </ChatHeader>
+      <ChatContainer>
+        <SearchMyChat userType={userType} />
+        <ChatList>
+          {userId && data
+            ? filterChats.length > 0
+              ? sortTime(filterChats).map((chat) => (
+                  <MyChatItem
+                    key={chat.id}
+                    name={chat.name}
+                    latestMessage={chat.latestMessage}
+                    users={chat.users}
+                    onClick={() => enterChatRoom(chat)}
+                    isPrivate={chat.isPrivate}
+                  />
+                ))
+              : sortTime(data).map((chat) => (
+                  <MyChatItem
+                    key={chat.id}
+                    name={chat.name}
+                    latestMessage={chat.latestMessage}
+                    users={chat.users}
+                    onClick={() => enterChatRoom(chat)}
+                    isPrivate={chat.isPrivate}
+                  />
+                ))
+            : null}
+        </ChatList>
+      </ChatContainer>
+    </Wrapper>
+  );
 };
 
 export default MyChats;
 
 export const Wrapper = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
 const ChatHeader = styled.div`
-    display: flex;
-    justify-content: space-between;
-    margin: 4rem 2rem 1rem;
+  display: flex;
+  justify-content: space-between;
+  margin: 4rem 2rem 1rem;
 `;
 
 const MyChatBar = styled.div`
-    color: #00956e;
-    font-weight: bold;
-    font-size: 1.5rem;
+  color: #00956e;
+  font-weight: bold;
+  font-size: 1.5rem;
 `;
 const IconBar = styled.div`
-    display: flex;
-    gap: 1.5rem;
+  display: flex;
+  gap: 1.5rem;
 `;
 
 const AddChatIcon = styled(AddChat)`
-    position: relative;
-    cursor: pointer;
+  position: relative;
+  cursor: pointer;
 `;
 
 const ChatContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    text-align: center;
-    margin: 2rem;
-    background-color: transparent;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  margin: 2rem;
+  background-color: transparent;
 `;
 
 const ChatList = styled.div`
-    overflow-y: auto;
-    height: 50rem;
-    &::-webkit-scrollbar {
-        /*크롬, 사파리, 오페라, 엣지*/
-        display: none;
-    }
-    -ms-overflow-style: none; /* ie */
-    scrollbar-width: none; /* 파이어폭스 */
+  overflow-y: auto;
+  height: 50rem;
+  &::-webkit-scrollbar {
+    /*크롬, 사파리, 오페라, 엣지*/
+    display: none;
+  }
+  -ms-overflow-style: none; /* ie */
+  scrollbar-width: none; /* 파이어폭스 */
 `;
--- src/components/chats/SearchMyChat.tsx
+++ src/components/chats/SearchMyChat.tsx
@@ -9,53 +9,53 @@ import { getAllChats, getMyChats } from './getChats';
 import { useQuery } from '@tanstack/react-query';
 
 const SearchMyChat = ({ userType }: any) => {
-    const [input, setInput] = useState<string>('');
-    const [filterChats, setFilteredChats] = useRecoilState(searchChatsState);
+  const [input, setInput] = useState<string>('');
+  const [filterChats, setFilteredChats] = useRecoilState(searchChatsState);
 
-    const { data: chats, isLoading } = useQuery<Chat[], any>({
-        queryKey: ['getChatsKey'],
-        queryFn: userType === 'my' ? getMyChats : getAllChats,
-        refetchOnWindowFocus: false,
-        staleTime: 10000,
-    });
-    const onInputChange = useCallback(
-        (e: React.ChangeEvent<HTMLInputElement>) => {
-            setInput(e.target.value);
-            if (!isLoading && chats) {
-                const filteringChats = chats.filter((chat) => {
-                    const filterValue = chat.name.toLowerCase().startsWith(e.target.value.toLowerCase());
-                    return filterValue;
-                });
-                console.log('Filtering Chats:', filteringChats);
-                const filteredChats = [...filteringChats];
-                setFilteredChats(filteredChats);
-            }
-        },
-        [isLoading, chats],
-    );
-    return (
-        <Wrapper>
-            <SearchUserBox>
-                <SearchButton>
-                    <MdSearch className="searchIcon" size="35" color="white" />
-                </SearchButton>
-                <SearchItem type="text" placeholder="검색" onChange={onInputChange} />
-            </SearchUserBox>
-        </Wrapper>
-    );
+  const { data: chats } = useQuery<Chat[], any>({
+    queryKey: ['getChatsKey'],
+    queryFn: userType === 'my' ? getMyChats : getAllChats,
+    refetchOnWindowFocus: false,
+    refetchInterval: 1000,
+  });
+  const onInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInput(e.target.value);
+      if (chats) {
+        const filteringChats = chats.filter((chat) => {
+          const filterValue = chat.name.toLowerCase().startsWith(e.target.value.toLowerCase());
+          return filterValue;
+        });
+        console.log('Filtering Chats:', filteringChats);
+        const filteredChats = [...filteringChats];
+        setFilteredChats(filteredChats);
+      }
+    },
+    [chats],
+  );
+  return (
+    <Wrapper>
+      <SearchUserBox>
+        <SearchButton>
+          <MdSearch className="searchIcon" size="35" color="white" />
+        </SearchButton>
+        <SearchItem type="text" placeholder="검색" onChange={onInputChange} />
+      </SearchUserBox>
+    </Wrapper>
+  );
 };
 
 export default SearchMyChat;
 
 const Wrapper = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: center;
+  width: 100%;
+  display: flex;
+  justify-content: center;
 `;
 
 const SearchItem = styled.input`
-    border: none;
-    width: 32rem;
-    outline: none;
-    font-size: 1.2rem;
+  border: none;
+  width: 32rem;
+  outline: none;
+  font-size: 1.2rem;
 `;
