a767f9826e973d383455a6eadb30949860abc24d
김소정 <kimsojeong@gimsojeong-ui-iMac.local>
2023-11-13 22:47:47 +0900

    ✨Feat: add signin

---
+++ src/api/login.tsx
@@ -0,0 +1,29 @@
+export const login = async (id, password) => {
+  const data = {
+    id: id,
+    password: password,
+  };
+  try {
+    const response = await fetch('https://fastcampus-chat.net/login', {
+      method: 'POST',
+      headers: {
+        serverId: '3440b401',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (response.ok) {
+      const json = await response.json();
+      console.log(json);
+      return json;
+    } else {
+      const errorJson = await response.json();
+      console.error('Login failed:', errorJson);
+      return { error: '아이디와 비밀번호를 다시 확인해주세요.' };
+    }
+  } catch (error) {
+    console.error('실패:', error);
+    return { error: '오류가 발생했습니다. 잠시 후 다시 시도해 주세요.' };
+  }
+};
--- src/pages/auth/Signin.tsx
+++ src/pages/auth/Signin.tsx
@@ -1,7 +1,70 @@
-import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import styles from '@styles/pages/signin.module.scss';
+import { login } from '@api/login';
+import { useForm } from '@hooks/useForm';
+import { useState } from 'react';
 
 const Signin = () => {
-  return <div>Signin</div>;
+  const [id, onChangeId] = useForm();
+  const [password, onChangePassword] = useForm();
+  const [errors, setErrors] = useState('');
+
+  // const navigate = useNavigate();
+  console.log('hello');
+
+  const signin = async (event) => {
+    event.preventDefault();
+
+    const result = await login(id, password);
+    console.log(result);
+
+    if (result.error) {
+      setErrors(result.error);
+    } else {
+      const { accessToken, refreshToken } = result;
+      localStorage.setItem('access_token', accessToken);
+      localStorage.setItem('refresh_token', refreshToken);
+      // navigate("/Loby");
+    }
+  };
+  return (
+    <div>
+      <div>
+        <div className={styles.main__background}>
+          <div>
+            <h1>Mafia</h1>
+          </div>
+          <div className={styles.signin_container}>
+            <form className={styles.signin_form} action="">
+              <input
+                placeholder="아이디를 입력해주세요."
+                type="text"
+                onChange={onChangeId}
+                value={id}
+              />
+              <input
+                placeholder="비밀번호를 입력해주세요."
+                type="password"
+                onChange={onChangePassword}
+                value={password}
+              />
+              {errors && <span className={styles.form_error}>{errors}</span>}
+
+              {/* 로그인 api */}
+              <button
+                className={styles.signin_btn}
+                onClick={(event) => signin(event)}>
+                로그인
+              </button>
+            </form>
+          </div>
+          <div className={styles.signup_link}>
+            <Link to="/signup">회원가입</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
