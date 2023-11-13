c77e24bf365933f7faa44101ded85b22d7b1dfa9
jisu Seo <jseo9732@gmail.com>
2023-11-13 17:28:16 +0900

    Feat: 채팅방 생성 기능 구현

--- apis/chatListAPI.ts
+++ apis/chatListAPI.ts
@@ -1,5 +1,11 @@
 import instance from './axios';
 
+interface ICreateChatRequestBody {
+  name: string; // chat 이름
+  users: string[]; // 참가자들 id(자신 미포함)
+  isPrivate?: boolean; // 공개 비공개
+}
+
 const chatListAPI = {
   // 모든 채팅 조회
   getAllChatList() {
@@ -8,6 +14,10 @@ const chatListAPI = {
   // 나의 채팅 조회
   getMyChatList() {
     return instance.get('/chat');
+  },
+  // 채팅방 생성
+  createChat(data: ICreateChatRequestBody) {
+    return instance.post('/chat', data);
   },
   // 채팅 참여
   participateChat(chatId: string) {
