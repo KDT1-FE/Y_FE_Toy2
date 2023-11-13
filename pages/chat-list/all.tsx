2254bb86d65044a4f937aa32d4c0bf8700015271
jisu Seo <jseo9732@gmail.com>
2023-11-13 17:47:55 +0900

    Comment: TODO 추가

--- pages/chat-list/all.tsx
+++ pages/chat-list/all.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { IChat } from '@/@types/types';
 import Image from 'next/image';
+import CreateChat from '@/components/ChatList/CreateChat';
 import chatListAPI from '../../apis/chatListAPI';
 import styles from './ChatList.module.scss';
 
@@ -18,13 +19,17 @@ export default function AllChatList() {
     getAllChat();
   }, []);
 
-  const participateChat = async e => {
-    await chatListAPI.participateChat(e.target.name);
-    router.push(`/chat/${e.target.name}`);
+  const participateChat = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (e.target instanceof HTMLButtonElement) {
+      await chatListAPI.participateChat(e.target.name);
+      router.push(`/chat/${e.target.name}`);
+    }
   };
   return (
     <>
+      <CreateChat />
       {allChatList.map(chat => (
+        // TODO|서지수  참여한 방이면 바로 이동하도록 수정
         // <Link
         //   href={`/chat/${chat.id}`}
         //   key={chat.id}
@@ -50,8 +55,9 @@ export default function AllChatList() {
             </div>
             <div>
               <div className={styles.chat_updated}>{chat.updatedAt}</div>
+              {/* TODO|서지수 내가 참여하지 않은 채팅방만 표시하도록 수정 */}
               <button type="button" name={chat.id} onClick={participateChat}>
-                aaa
+                참여
               </button>
             </div>
           </div>
