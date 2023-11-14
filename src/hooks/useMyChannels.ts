2d0d7312e8a0127796446edcac24fd67160fb284
SKY-PEY <lovr8537@gmail.com>
2023-11-14 13:31:50 +0900

    fix: 이전 커밋에서 삭제 된 타입 추가

--- src/hooks/useMyChannels.ts
+++ src/hooks/useMyChannels.ts
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { getMyChannels } from '../api/channel';
+import { Channel } from '../@types/channel';
 
 export const useMyChannels = () => {
-  return useQuery({
+  return useQuery<Channel[]>({
     queryKey: ['my-channels'],
     queryFn: getMyChannels,
   });
