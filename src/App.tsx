aecc89a618850e9ab5c9edec013e70858565ac7d
89882 <pgh123166@naver.com>
2023-11-14 12:17:41 +0900

    feat: join route 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Layout from './layout';
 import ChannelPage from './pages/channel';
 import Chats from './pages/chats';
+import Join from './pages/join';
 function App() {
   return (
     <Router>
@@ -9,9 +10,9 @@ function App() {
         <Route element={<Layout />}>
           <Route path="/chats" element={<Chats />} />
         </Route>
+        <Route path="/join" element={<Join />} />
       </Routes>
     </Router>
-
   );
 }
 
