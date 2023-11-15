fdcd2d9c4a4503ee10933abe282adff36ec8dbe2
developer-jyyun <developer.jyyun@gmail.com>
2023-11-15 15:08:40 +0900

    Feat: 하단 네비게이션 관련 recoil 파일 생성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
+import { RecoilRoot } from 'recoil';
+import type { AppProps } from 'next/app';
+import Navigation from '@/components/Navigation/Navigation';
 import '../styles/normalize.scss';
 import '../assets/fonts/fonts.scss';
-import type { AppProps } from 'next/app';
-import { RecoilRoot } from 'recoil';
-import Navigation from '@/components/Navigation/Navigation';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
