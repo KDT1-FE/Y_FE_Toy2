d794632c5db7bb869c7d462ae22636b0d1f43112
sunpaaaa <sunpaaaa@gmail.com>
2023-11-16 00:20:22 +0900

    💄Design: add stylesheet for night page

--- src/pages/game/Night.tsx
+++ src/pages/game/Night.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styles from '@styles/pages/night.module.scss';
+import GameHeader from '@components/common/GameHeader';
 
 const Night = () => {
-  return <div>Night</div>;
+  return (
+    <div className={styles.night}>
+      <GameHeader started={true} title="" timer={5} />
+      <div className={styles.night__text}>
+        <span>
+          밤이 <br /> 되었습니다...
+        </span>
+      </div>
+      <div className={styles.night__img}></div>
+    </div>
+  );
 };
 
 export default Night;
