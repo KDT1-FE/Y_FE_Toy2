458002694e80b515437ed5404a0d819def5d3e86
jimin <moana16@naver.com>
2023-11-15 21:58:52 +0900

    style : 헤더 추가

---
+++ components/Header/Header.tsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import style from './Header.module.scss';
+
+export default function Header() {
+  const [shrink, setShrink] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) { // 스크롤 위치에 따라 값을 조정하세요
+      setShrink(true);
+    } else {
+      setShrink(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className={`${style.container} ${shrink ? style.shrink : ''}`}>
+      <Image
+        src="/images/Talkhaja.svg"
+        alt="talkhaja_logo"
+        width={shrink ? 120 : 170} // 축소될 때의 크기
+        height={shrink ? 42 : 60} // 축소될 때의 크기
+        style={{ transition: 'all 0.3s ease' }}
+      />
+    </div>
+  );
+}
