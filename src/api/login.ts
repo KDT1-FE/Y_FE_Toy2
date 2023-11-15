d6f733917b2b9a5ef9698245cb2edd412d66f97b
zoeyourlife <201814112@office.skhu.ac.kr>
2023-11-15 13:49:55 +0900

    feat: 로그인 api 연동

---
+++ src/api/login.ts
@@ -0,0 +1,33 @@
+import { LoginForm } from '../@types/login';
+import instance from './axios';
+
+interface RequestBody {
+  id: string;
+  password: string;
+}
+
+interface ResponseValue {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export const login = async (LoginForm: LoginForm) => {
+  const { id, password } = LoginForm;
+
+  const requestBody: RequestBody = {
+    id,
+    password,
+  };
+
+  try {
+    const response = await instance.post<ResponseValue>(
+      'https://fastcampus-chat.net/login',
+      requestBody,
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error('Error during login:', error);
+    throw error;
+  }
+};
