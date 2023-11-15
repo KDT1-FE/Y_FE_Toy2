ea28b11738e7ef9509c286f06a7021fac8fc6c23
Eojoonhyuk <anftlcb@gmail.com>
2023-11-16 02:14:20 +0900

    Fix: instance 수정

--- apis/chatListAPI.ts
+++ apis/chatListAPI.ts
@@ -1,10 +1,12 @@
-import instance from './axios';
+import Jwtinterceptors from './JwtInterceptor';
 
 interface ICreateChatRequestBody {
   name: string; // chat 이름
   users: string[]; // 참가자들 id(자신 미포함)
   isPrivate?: boolean; // 공개 비공개
 }
+
+const { instance } = Jwtinterceptors();
 
 const chatListAPI = {
   // 모든 채팅 조회
