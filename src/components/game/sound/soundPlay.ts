6a032f7d6ecfee9de3adf23274dc7911c9dcc60b
Yamyam-code <rnffjt@gmail.com>
2023-11-15 22:01:16 +0900

    Fix: 음원 이름 수정

--- src/components/game/sound/soundPlay.ts
+++ src/components/game/sound/soundPlay.ts
@@ -1,19 +1,19 @@
 import React from 'react';
-import 성공 from './source/성공.mp3';
-import 실패 from './source/실패.mp3';
-import 시작 from './source/시작.mp3';
-import 종료 from './source/종료.mp3';
+import success from './source/success.mp3';
+import error from './source/error.mp3';
+import start from './source/start.mp3';
+import end from './source/end.mp3';
 
 function soundPlay(situation: string) {
   let audio: HTMLAudioElement | undefined;
   if (situation === 'success') {
-    audio = new Audio(성공);
+    audio = new Audio(success);
   } else if (situation === 'error') {
-    audio = new Audio(실패);
+    audio = new Audio(error);
   } else if (situation === 'start') {
-    audio = new Audio(시작);
+    audio = new Audio(start);
   } else if (situation === 'end') {
-    audio = new Audio(종료);
+    audio = new Audio(end);
   }
   if (audio) {
     audio.play();
