c725aa71f4b525f93a20ba6e816135b6f30cd25f
JinJeongMin <jdsjm2@naver.com>
2023-11-15 10:13:20 +0900

    Feat: socket 에러 해결, 유저 블락 구현

--- src/components/chating/ChatingModal.tsx
+++ src/components/chating/ChatingModal.tsx
@@ -7,186 +7,184 @@ import { useRouter } from 'next/navigation';
 import InviteImg from '../../../public/assets/InviteImg.svg';
 
 interface User {
-    username: string;
-    id: string;
-    picture: string;
+  username: string;
+  id: string;
+  picture: string;
 }
 
 interface ChatingModalProps {
-    users: User[];
-    chatId: string;
+  users: User[];
+  chatId: string;
 }
 
 //type
 export default function ChatingModal(props: ChatingModalProps) {
-    const [modalToggle, setModalToggle] = useRecoilState<boolean>(ChatingModalToggle);
-    const router = useRouter();
+  const [modalToggle, setModalToggle] = useRecoilState<boolean>(ChatingModalToggle);
+  const router = useRouter();
 
-    const accessToken = typeof window !== 'undefined' ? sessionStorage.getItem('accessToken') : null;
+  const accessToken = typeof window !== 'undefined' ? sessionStorage.getItem('accessToken') : null;
 
-    const leaveChating = async () => {
-        await fetch('https://fastcampus-chat.net/chat/leave', {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${accessToken}`,
-                serverId: `${process.env.NEXT_PUBLIC_SERVER_KEY}`,
-            },
-            body: JSON.stringify({
-                chatId: props.chatId,
-            }),
-        });
-        router.back();
-    };
+  const leaveChating = async () => {
+    await fetch('https://fastcampus-chat.net/chat/leave', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+        serverId: `${process.env.NEXT_PUBLIC_SERVER_KEY}`,
+      },
+      body: JSON.stringify({
+        chatId: props.chatId,
+      }),
+    });
+    router.back();
+  };
 
-    console.log(props, 'props');
+  return (
+    <>
+      <ModalWrapper
+        style={{
+          opacity: `${modalToggle ? '1' : '0'}`,
+          transform: `${modalToggle ? 'translateZ(0)' : 'translate3d(100%, 0, 0)'}`,
+        }}
+      >
+        <ModalTitle>대화상대</ModalTitle>
+        {props.users ? (
+          <UsersWrapper>
+            {props.users.map((user: User) => (
+              <UserWrapper key={user.id}>
+                <UserImg src={user.picture} />
+                <UserName>{user.username}</UserName>
+              </UserWrapper>
+            ))}
+            <UserInviteWrapper>
+              <UserInviteImg />
+              <UserInviteName>초대하기</UserInviteName>
+            </UserInviteWrapper>
+          </UsersWrapper>
+        ) : (
+          ''
+        )}
+        <ChatingLeave
+          onClick={() => {
+            setModalToggle(!modalToggle);
+            leaveChating();
+          }}
+        >
+          채팅방 나가기
+        </ChatingLeave>
+      </ModalWrapper>
 
-    return (
-        <>
-            <ModalWrapper
-                style={{
-                    opacity: `${modalToggle ? '1' : '0'}`,
-                    transform: `${modalToggle ? 'translateZ(0)' : 'translate3d(100%, 0, 0)'}`,
-                }}
-            >
-                <ModalTitle>대화상대</ModalTitle>
-                {props.users ? (
-                    <UsersWrapper>
-                        {props.users.map((user: User) => (
-                            <UserWrapper>
-                                <UserImg src={user.picture} />
-                                <UserName>{user.username}</UserName>
-                            </UserWrapper>
-                        ))}
-                        <UserInviteWrapper>
-                            <UserInviteImg />
-                            <UserInviteName>초대하기</UserInviteName>
-                        </UserInviteWrapper>
-                    </UsersWrapper>
-                ) : (
-                    ''
-                )}
-                <ChatingLeave
-                    onClick={() => {
-                        setModalToggle(!modalToggle);
-                        leaveChating();
-                    }}
-                >
-                    채팅방 나가기
-                </ChatingLeave>
-            </ModalWrapper>
-
-            <ModalBackground
-                style={{
-                    visibility: `${modalToggle ? 'visible' : 'hidden'}`,
-                    opacity: `${modalToggle ? '1' : '0'}`,
-                }}
-                onClick={() => setModalToggle(!modalToggle)}
-            ></ModalBackground>
-        </>
-    );
+      <ModalBackground
+        style={{
+          visibility: `${modalToggle ? 'visible' : 'hidden'}`,
+          opacity: `${modalToggle ? '1' : '0'}`,
+        }}
+        onClick={() => setModalToggle(false)}
+      ></ModalBackground>
+    </>
+  );
 }
 
 const ModalBackground = styled.div`
-    width: 100%;
-    height: 100vh;
+  width: 100%;
+  height: 100vh;
 
-    position: absolute;
-    z-index: 2;
+  position: absolute;
+  z-index: 2;
 
-    background-color: rgba(0, 0, 0, 0.2);
+  background-color: rgba(0, 0, 0, 0.2);
 
-    transition: all 0.5s;
+  transition: all 0.5s;
 `;
 
 const ModalWrapper = styled.div`
-    width: 70%;
-    height: 100%;
+  width: 70%;
+  height: 100%;
 
-    background-color: #fff;
+  background-color: #fff;
 
-    position: absolute;
-    right: 0;
+  position: absolute;
+  right: 0;
 
-    z-index: 3;
+  z-index: 3;
 
-    transition: 0.5s;
+  transition: 0.5s;
 `;
 
 const ModalTitle = styled.div`
-    width: 100%;
-    font-size: 24px;
+  width: 100%;
+  font-size: 24px;
 
-    padding-left: 20px;
-    padding-top: 20px;
+  padding-left: 20px;
+  padding-top: 20px;
 
-    font-weight: 700;
+  font-weight: 700;
 `;
 
 const UsersWrapper = styled.div`
-    width: 100%;
-    height: 80%;
+  width: 100%;
+  height: 80%;
 
-    overflow: scroll;
-    padding-left: 30px;
-    padding-top: 30px;
+  overflow: scroll;
+  padding-left: 30px;
+  padding-top: 30px;
 `;
 
 const UserWrapper = styled.div`
-    width: 100%;
+  width: 100%;
 
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 
-    margin-bottom: 20px;
+  margin-bottom: 20px;
 `;
 
 const UserImg = styled.img`
-    width: 50px;
-    height: 50px;
+  width: 50px;
+  height: 50px;
 
-    border-radius: 25px;
+  border-radius: 25px;
 `;
 
 const UserName = styled.div`
-    font-size: 24px;
-    margin-left: 10px;
+  font-size: 24px;
+  margin-left: 10px;
 `;
 
 const UserInviteWrapper = styled.div`
-    width: 100%;
+  width: 100%;
 
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 
-    margin-bottom: 20px;
+  margin-bottom: 20px;
 `;
 
 const UserInviteImg = styled(InviteImg)`
-    width: 50px;
-    height: 50px;
+  width: 50px;
+  height: 50px;
 
-    border-radius: 25px;
-    cursor: pointer;
+  border-radius: 25px;
+  cursor: pointer;
 `;
 
 const UserInviteName = styled.div`
-    font-size: 24px;
-    margin-left: 10px;
+  font-size: 24px;
+  margin-left: 10px;
 
-    color: #00956e;
-    cursor: pointer;
+  color: #00956e;
+  cursor: pointer;
 `;
 
 const ChatingLeave = styled.div`
-    font-size: 30px;
-    color: #950000;
+  font-size: 30px;
+  color: #950000;
 
-    position: absolute;
-    bottom: 0;
+  position: absolute;
+  bottom: 0;
 
-    left: 50%;
-    transform: translate(-50%, -50%);
+  left: 50%;
+  transform: translate(-50%, -50%);
 
-    cursor: pointer;
+  cursor: pointer;
 `;
--- src/components/chating/ChatingPage.tsx
+++ src/components/chating/ChatingPage.tsx
@@ -4,275 +4,267 @@ import styled from 'styled-components';
 import React, { useEffect, useState } from 'react';
 import MessageContainer from './MessageContainer';
 import io from 'socket.io-client';
-import { usePathname } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import ChatingNavigation from './ChatingNavigation';
 import ChatingModal from './ChatingModal';
 import { formatCreatedAt } from '../chats/useFormatCreatedAt';
 
 interface Message {
-    id: string;
-    text: string;
-    userId: string;
-    createdAt: Date; // Date
+  id: string;
+  text: string;
+  userId: string;
+  createdAt: Date; // Date
 }
 
 interface User {
-    username: string;
-    id: string;
-    picture: string;
+  username: string;
+  id: string;
+  picture: string;
 }
 
 export default function ChatingPage() {
-    const [messages, setMessages] = useState<Message[]>([]);
-    const [users, setUsers] = useState<User[]>([]);
-    const [chatName, setChatName] = useState<string>('');
-    const [getUserToggle, setGetUserToggle] = useState<boolean>(true);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
+  const [chatName, setChatName] = useState<string>('');
+  const [getUserToggle, setGetUserToggle] = useState<boolean>(true);
 
-    const pathname = usePathname();
-    const chatId = pathname.split('/')[2];
-    const accessToken = typeof window !== 'undefined' ? sessionStorage.getItem('accessToken') : null;
-    const userId = typeof window !== 'undefined' ? sessionStorage.getItem('userId') : null;
+  const router = useRouter();
 
-    const getUsers = async () => {
-        const response = await fetch('https://fastcampus-chat.net/chat', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${accessToken}`,
-                serverId: `${process.env.NEXT_PUBLIC_SERVER_KEY}`,
-            },
-        });
-        const data = await response.json();
+  const pathname = usePathname();
+  const chatId = pathname.split('/')[2];
+  const accessToken = typeof window !== 'undefined' ? sessionStorage.getItem('accessToken') : null;
+  const userId = typeof window !== 'undefined' ? sessionStorage.getItem('userId') : null;
 
-        for (let i = 0; i < data.chats.length; i++) {
-            if (chatId == data.chats[i].id) {
-                setChatName(data.chats[i].name);
-                setUsers(data.chats[i].users);
-            }
-        }
-    };
-
-    const findUserName = (userId: string): string | void => {
-        for (let i = 0; i < users.length; i++) {
-            if (userId == users[i].id) {
-                return users[i].username;
-            }
-        }
-    };
-
-
-    const findUserPicture = (userId: string): string | void => {
-        for (let i = 0; i < users.length; i++) {
-            if (userId == users[i].id) {
-                return users[i].picture;
-            }
-        }
-    };
-
-    useEffect(() => {
-        getUsers();
-    }, [getUserToggle]);
-
-    useEffect(() => {
-        socketInitilizer();
-    }, []);
-
-    const socket = io(`wss://fastcampus-chat.net/chat?chatId=${chatId}`, {
-        extraHeaders: {
-            Authorization: `Bearer ${accessToken}`,
-            serverId: `${process.env.NEXT_PUBLIC_SERVER_KEY}`,
-        },
+  const getUsers = async () => {
+    const response = await fetch('https://fastcampus-chat.net/chat', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+        serverId: `${process.env.NEXT_PUBLIC_SERVER_KEY}`,
+      },
     });
+    const data = await response.json();
 
-    const socketInitilizer = () => {
-        socket.on('connect', () => {
-            console.log('Socket connected');
-        });
+    for (let i = 0; i < data.chats.length; i++) {
+      if (chatId == data.chats[i].id) {
+        setChatName(data.chats[i].name);
+        setUsers(data.chats[i].users);
+      }
+    }
+  };
 
-        socket.emit('fetch-messages');
+  const findUserName = (userId: string): string | undefined => {
+    for (let i = 0; i < users.length; i++) {
+      if (userId == users[i].id) {
+        return users[i].username;
+      }
+    }
+    return undefined;
+  };
 
-        socket.on('messages-to-client', (messageObject) => {
-            setMessages(messageObject.messages.reverse());
-        });
+  const findUserPicture = (userId: string): string | undefined => {
+    for (let i = 0; i < users.length; i++) {
+      if (userId == users[i].id) {
+        return users[i].picture;
+      }
+    }
+    return undefined;
+  };
 
-        socket.on('message-to-client', (messageObject) => {
-            setMessages((prevMessages) => [messageObject, ...prevMessages]);
-        });
+  const socket = io(`wss://fastcampus-chat.net/chat?chatId=${chatId}`, {
+    extraHeaders: {
+      Authorization: `Bearer ${accessToken}`,
+      serverId: `${process.env.NEXT_PUBLIC_SERVER_KEY}`,
+    },
+  });
 
-        socket.on('disconnect', () => {
-            console.log('Socket disconnected');
-        });
+  useEffect(() => {
+    getUsers();
+  }, [getUserToggle]);
 
-        socket.emit('users');
+  useEffect(() => {
+    try {
+      socket.on('connect', () => {
+        console.log('Socket connected');
+        setTimeout(() => {
+          socket.emit('fetch-messages');
+          console.log(1);
+        }, 500);
+      });
+      socket.on('disconnect', () => {
+        console.log('disconnect');
+        router.back();
+      });
 
-        // socket.on('users-to-client', (data) => {
-        //     console.log(data, 'users-to-client');
-        // });
+      socket.emit('fetch-messages');
 
-        socket.on('join', (data) => {
-            console.log(data, 'join');
-            setGetUserToggle(!getUserToggle);
-        });
-        socket.on('leave', (data) => {
-            console.log(data, 'leave');
-            setGetUserToggle(!getUserToggle);
-        });
-    };
+      socket.on('messages-to-client', (messageObject) => {
+        console.log(messageObject);
+        setMessages(messageObject.messages.reverse());
+      });
 
-    return (
-        <main>
-            <ChatingNavigation chatName={chatName} />
-            <ChatingModal users={users} chatId={chatId} />
+      socket.on('message-to-client', (messageObject) => {
+        setMessages((prevMessages) => [messageObject, ...prevMessages]);
+      });
 
-            <MessagesContainer>
-                {messages
-                    ? messages.map((message: Message, i: number) =>
-                          userId == message.userId.split(':')[1] || userId == message.userId.split(':')[0] ? (
-                              <MyMessageWrapper key={message.id}>
-                                  <MyMessageText>{message.text}</MyMessageText>
-                                  <MyMessageTime>{formatCreatedAt(message.createdAt)}</MyMessageTime>
-                              </MyMessageWrapper>
-                          ) : messages[i].userId == messages[i + 1]?.userId ||
-                            messages[i].userId == messages[i + 1]?.userId.split(':')[1] ? (
-                              <YourMessageWrapper key={message.id}>
-                                  <YourMessageTextWrapper>
-                                      <YourMessageText>{message.text}</YourMessageText>
-                                      <YourMessageTime>{formatCreatedAt(message.createdAt)}</YourMessageTime>
-                                  </YourMessageTextWrapper>
-                              </YourMessageWrapper>
-                          ) : (
-                              <YourMessageWrapper key={message.id}>
-                                  <YourMessageNameWrapper>
-                                      <YourMessagePicture
-                                          src={
-                                              findUserPicture(
-                                                  message.userId.split(':')[message.userId.split(':').length - 1],
+      // socket.emit('users');
 
-                                              )
-                                                  ? 'https://gravatar.com/avatar/0211205be1e2bce90bbe53c5e0d8aaff?s=200&d=retro'
-                                                  : findUserPicture(
-                                                        message.userId.split(':')[message.userId.split(':').length - 1],
-                                                    ) || ''
-                                          }
-                                      />
-                                      <YourMessageName>
-                                          {findUserName(message.userId.split(':')[message.userId.split(':').length - 1])
-                                              ? '(퇴장한 사용자)'
-                                              : findUserName(
-                                                    message.userId.split(':')[message.userId.split(':').length - 1],
-                                                ) || ''}
+      // socket.on('users-to-client', (data) => {
+      //     console.log(data, 'users-to-client');
+      // });
 
-                                      </YourMessageName>
-                                  </YourMessageNameWrapper>
-                                  <YourMessageTextWrapper>
-                                      <YourMessageText>{message.text}</YourMessageText>
-                                      <YourMessageTime>{formatCreatedAt(message.createdAt)}</YourMessageTime>
-                                  </YourMessageTextWrapper>
-                              </YourMessageWrapper>
-                          ),
-                      )
-                    : ''}
-            </MessagesContainer>
-            <MessageContainer socket={socket} />
-        </main>
-    );
+      socket.on('join', (data) => {
+        console.log(data, 'join');
+        setGetUserToggle(!getUserToggle);
+      });
+      socket.on('leave', (data) => {
+        console.log(data, 'leave');
+        setGetUserToggle(!getUserToggle);
+      });
+      return () => {
+        socket.disconnect();
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  return (
+    <main>
+      <ChatingNavigation chatName={chatName} />
+      <ChatingModal users={users} chatId={chatId} />
+
+      <MessagesContainer>
+        {messages
+          ? messages.map((message: Message, i: number) =>
+              userId == message.userId || userId == message.userId ? (
+                <MyMessageWrapper key={message.id}>
+                  <MyMessageText>{message.text}</MyMessageText>
+                  <MyMessageTime>{formatCreatedAt(message.createdAt)}</MyMessageTime>
+                </MyMessageWrapper>
+              ) : messages[i].userId == messages[i + 1]?.userId || messages[i].userId == messages[i + 1]?.userId ? (
+                <YourMessageWrapper key={message.id}>
+                  <YourMessageTextWrapper>
+                    <YourMessageText>{message.text}</YourMessageText>
+                    <YourMessageTime>{formatCreatedAt(message.createdAt)}</YourMessageTime>
+                  </YourMessageTextWrapper>
+                </YourMessageWrapper>
+              ) : (
+                <YourMessageWrapper key={message.id}>
+                  <YourMessageNameWrapper>
+                    <YourMessagePicture src={findUserPicture(message.userId)} />
+                    <YourMessageName>{findUserName(message.userId) || ''}</YourMessageName>
+                  </YourMessageNameWrapper>
+                  <YourMessageTextWrapper>
+                    <YourMessageText>{message.text}</YourMessageText>
+                    <YourMessageTime>{formatCreatedAt(message.createdAt)}</YourMessageTime>
+                  </YourMessageTextWrapper>
+                </YourMessageWrapper>
+              ),
+            )
+          : ''}
+      </MessagesContainer>
+      <MessageContainer socket={socket} />
+    </main>
+  );
 }
 
 const MessagesContainer = styled.div`
-    width: 100%;
-    height: 100vh;
+  width: 100%;
+  height: 100vh;
 
-    padding-bottom: 83px;
-    background-color: #eee;
+  padding-bottom: 83px;
+  background-color: #eee;
 
-    display: flex;
-    flex-direction: column-reverse;
+  display: flex;
+  flex-direction: column-reverse;
 
-    overflow: scroll;
+  overflow: scroll;
 `;
 
 const YourMessageWrapper = styled.div`
-    width: 100%;
+  width: 100%;
 `;
 
 const YourMessageNameWrapper = styled.div`
-    width: 100%;
-    display: flex;
-    margin-bottom: 10px;
+  width: 100%;
+  display: flex;
+  margin-bottom: 10px;
 `;
 
 const YourMessageName = styled.div`
-    font-size: 24px;
-    color: #000;
+  font-size: 24px;
+  color: #000;
 
-    display: flex;
-    flex-direction: column;
-    margin-left: 5px;
+  display: flex;
+  flex-direction: column;
+  margin-left: 5px;
 `;
 
 const YourMessagePicture = styled.img`
-    width: 40px;
-    height: 40px;
+  width: 40px;
+  height: 40px;
 
-    border-radius: 20px;
+  border-radius: 20px;
 `;
 
 const YourMessageTextWrapper = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: row;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
 
-    margin-bottom: 10px;
+  margin-bottom: 10px;
 `;
 const YourMessageText = styled.div`
-    max-width: 75%;
-    padding: 10px;
+  max-width: 75%;
+  padding: 10px;
 
-    margin-left: 40px;
+  margin-left: 40px;
 
-    font-size: 16px;
+  font-size: 16px;
 
-    border-radius: 15px;
+  border-radius: 15px;
 
-    background-color: #d9d9d9;
+  background-color: #d9d9d9;
 `;
 
 const YourMessageTime = styled.div`
-    color: #888;
-    font-size: 12px;
+  color: #888;
+  font-size: 12px;
 
-    margin-left: 5px;
+  margin-left: 5px;
 
-    display: flex;
-    flex-direction: column-reverse;
+  display: flex;
+  flex-direction: column-reverse;
 `;
 
 const MyMessageWrapper = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: row-reverse;
-    margin-bottom: 10px;
+  width: 100%;
+  display: flex;
+  flex-direction: row-reverse;
+  margin-bottom: 10px;
 `;
 
 const MyMessageText = styled.div`
-    max-width: 75%;
-    padding: 10px;
+  max-width: 75%;
+  padding: 10px;
 
-    margin-right: 10px;
+  margin-right: 10px;
 
-    font-size: 16px;
+  font-size: 16px;
 
-    border-radius: 15px;
+  border-radius: 15px;
 
-    background-color: #00956e;
+  background-color: #00956e;
 `;
 
 const MyMessageTime = styled.div`
-    color: #888;
-    font-size: 12px;
+  color: #888;
+  font-size: 12px;
 
-    margin-right: 5px;
+  margin-right: 5px;
 
-    display: flex;
-    flex-direction: column-reverse;
+  display: flex;
+  flex-direction: column-reverse;
 `;
