f1fd6759b124f4dcf742696c055d40adf81b0ccb
developer-jyyun <developer.jyyun@gmail.com>
2023-11-15 22:43:33 +0900

    Fix: merge conflicts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot, RecoilEnv } from 'recoil';
 import type { AppProps } from 'next/app';
 import Navigation from '@/components/Navigation/Navigation';
-import '../styles/normalize.scss';
-import '../assets/fonts/fonts.scss';
+import '@/styles/normalize.scss';
+import '@/assets/fonts/fonts.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
+  RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
   return (
     <RecoilRoot>
       <Navigation />
