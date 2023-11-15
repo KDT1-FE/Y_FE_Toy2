458e3c6f2b565e463db5dce7513cab192ff3e44d
zoeyourlife <201814112@office.skhu.ac.kr>
2023-11-15 13:53:33 +0900

    feat: login 페이지 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Chats from './pages/chats';
 import MyChannels from './components/channel/MyChannels';
 import ChannelList from './components/channel';
 import Join from './pages/join';
+import Login from './pages/login';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route element={<Layout />}>
           <Route path="/" element={<ChannelPage />}>
             <Route path="/" element={<ChannelList />} />
