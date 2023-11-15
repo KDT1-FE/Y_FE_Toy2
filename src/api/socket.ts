a52531dfe386d7c836bae6ccdacc2d7adaaf722e
zoeyourlife <201814112@office.skhu.ac.kr>
2023-11-15 16:41:22 +0900

    feat: socket 헤더 부분 토큰 제거

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 const getSocket = (chatId: string) => {
   const socket = io(`${process.env.REACT_APP_API_URL}/chat?chatId=${chatId}`, {
     extraHeaders: {
-      Authorization: `Bearer ${process.env.REACT_APP_USER_TOKEN}`,
+      Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
       serverId: `${process.env.REACT_APP_SERVER_ID}`,
     },
   });
