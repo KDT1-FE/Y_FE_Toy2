e2c948248effc97511e30cb1224e88f05a5ab1db
TaePoong719 <just5538@gmail.com>
2023-11-16 11:38:49 +0900

    Feat : Users 페이지 디자인 작업

--- Components/Users/FriendProfiles.tsx
+++ Components/Users/FriendProfiles.tsx
@@ -33,37 +33,33 @@ export const FriendProfile = ({ user }: { user: User | UserHasOnline }) => {
 	const isUserOnline = 'isOnline' in user;
 
 	return (
-		<div className="flex w-full align-center mb-4">
+		<div className="flex w-full align-center mb-1">
 			<div>
 				<div className="relative">
-					<div className="relative w-14 h-14 mr-5">
+					<div className="relative w-12 h-12 mr-5">
 						<Image
 							fill={true}
 							alt={user.name}
 							src={picture}
-							className="rounded-3xl border-black"
+							className="absolute top-0 left-0 rounded-3xl border-black my-2 mx-auto"
 							style={{
-								position: 'absolute',
-								top: 0,
-								left: 0,
+								filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
 								objectFit: 'cover',
-								margin: 'auto',
 							}}
 						/>
 					</div>
 					{isUserOnline && (
 						<span className="absolute top-0 left-2/3">
-							<Image
-								src={`/icon_${user.isOnline ? 'green' : 'gray'}_dot.svg`}
-								height={15}
-								width={15}
-								alt="online"
-							/>
+							<div
+								className={`w-3.5 h-3.5 rounded-full bg-${
+									user.isOnline ? 'primary' : 'gray-500'
+								} `}
+							></div>
 						</span>
 					)}
 				</div>
 			</div>
-			<h4 className="w-1/2 py-4 text-l">{user.name}</h4>
+			<h4 className="w-1/2 py-4 mx-4 text-l">{user.name}</h4>
 		</div>
 	);
 };
