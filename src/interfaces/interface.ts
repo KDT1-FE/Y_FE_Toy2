0ecf9963644334e34c5ba196b44b614cf65b1ec4
Hojin <wkdghwls0623@gmail.com>
2023-11-16 15:24:01 +0900

    Chore: pull 전 저장

--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -1,8 +1,9 @@
+//interface.ts
 export interface JoinData {
   id: string;
   password: string;
   name: string;
-  picture?: string; 
+  picture?: string;
 }
 
 export interface FormData extends JoinData {
@@ -13,4 +14,28 @@ export interface ValidationInput {
   fieldName: keyof FormData;
   value: string;
   formData: FormData;
+}
+
+export interface ChatResponse {
+  chats: Chat[];
+}
+
+export interface OnlyResponse {
+  chat: Chat[];
+}
+
+export interface Chat {
+  id: string;
+  name: string;
+  users: User[]; // 속한 유저 id
+  isPrivate: boolean;
+  updatedAt: Date;
+  createdAt: Date;
+  index: number;
+}
+
+export interface User {
+  id: string;
+  name: string;
+  picture: string;
 }
