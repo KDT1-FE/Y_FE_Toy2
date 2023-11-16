b2799f9eab3811b171983267cac14ec51a144056
JitHoon <imessi0336@gmail.com>
2023-11-16 12:15:59 +0900

    Feat: 채팅방 디자인 추가 업데이트

--- Components/Chat/RenderChats.tsx
+++ Components/Chat/RenderChats.tsx
@@ -29,40 +29,38 @@ const RenderChats = ({
 	}, [messages.length]);
 
 	return (
-		<>
-			<ul className="w-full flex flex-col items-center">
-				{messages.map((message, index) => {
-					const myUser = chatUsers.find((user) => user.id === message.userId);
+		<ul className="w-full flex flex-col items-center mt-4 mb-16">
+			{messages.map((message, index) => {
+				const myUser = chatUsers.find((user) => user.id === message.userId);
 
-					if (myUser) {
-						return (
-							<React.Fragment key={message.id}>
-								{index === 0 ||
-								new Date(message.createdAt).toDateString() !==
-									new Date(messages[index - 1].createdAt).toDateString() ? (
-									<li
-										key={`${message.id}-date`} // 고유한 키를 생성
-										className="my-2 w-[130px] flex justify-center items-center h-8 bg-gray-200 rounded-2xl"
-									>
-										<span className="text-xs">
-											{new Date(message.createdAt).toDateString()}
-										</span>
-									</li>
-								) : null}
-								<Chats
-									key={message.id}
-									message={message}
-									user={myUser}
-									myId={myId}
-									useModal={useModal}
-								/>
-							</React.Fragment>
-						);
-					}
-				})}
-				<div ref={messageEndRef}></div>
-			</ul>
-		</>
+				if (myUser) {
+					return (
+						<React.Fragment key={message.id}>
+							{index === 0 ||
+							new Date(message.createdAt).toDateString() !==
+								new Date(messages[index - 1].createdAt).toDateString() ? (
+								<li
+									key={`${message.id}-date`}
+									className="my-2 w-[130px] flex justify-center items-center h-8 bg-gray-200 rounded-2xl shadow-gray-300 shadow-md"
+								>
+									<span className="text-xs">
+										{new Date(message.createdAt).toDateString()}
+									</span>
+								</li>
+							) : null}
+							<Chats
+								key={message.id}
+								message={message}
+								user={myUser}
+								myId={myId}
+								useModal={useModal}
+							/>
+						</React.Fragment>
+					);
+				}
+			})}
+			<div ref={messageEndRef}></div>
+		</ul>
 	);
 };
 export default RenderChats;
