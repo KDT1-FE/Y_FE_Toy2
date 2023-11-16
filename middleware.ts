56b58c2e410b34f8ada11c9b321f028355380c88
TaePoong719 <just5538@gmail.com>
2023-11-16 18:09:01 +0900

    Chore : nextjs 13.5.3 버젼으로 다운그레이드, middleware 적용 해제

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,12 @@ import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 export const middleware = (request: NextApiRequest) => {
-	const cookieStore = cookies();
-	const cookie = cookieStore.get('accessToken');
-	if (cookie) {
-		return NextResponse.next();
-	}
-	return NextResponse.redirect(new URL('/login', request.url));
+	// const cookieStore = cookies();
+	// const cookie = cookieStore.get('accessToken');
+	// if (cookie) {
+	return NextResponse.next();
+	// }
+	// return NextResponse.redirect(new URL('/login', request.url));
 };
 
 export const config = {
