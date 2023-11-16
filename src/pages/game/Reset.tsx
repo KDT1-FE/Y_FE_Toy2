cb75a5009d503aa897d7376751be41becaaa5661
sunpaaaa <sunpaaaa@gmail.com>
2023-11-16 17:21:06 +0900

    💄Design: add reset page

---
+++ src/pages/game/Reset.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '@styles/pages/reset.module.scss';
+
+const Reset = () => {
+  return (
+    <div className={styles.reset}>
+      <p>
+        밤 동안 <span>아무 일도</span> <br />
+        일어나지 않았습니다...
+      </p>
+    </div>
+  );
+};
+
+export default Reset;
