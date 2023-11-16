ab069a89ac732883da6475929a1aca36f4cead84
mysdpy30s <mysdpy30s@gmail.com>
2023-11-16 10:48:32 +0900

    Update: Deleted 'UserHobby' folder and code update

--- src/components/UserHobby/UserHobby.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-function UserHobby({ hobbyName }) {
-  return <UserHobbyContainer>{hobbyName}</UserHobbyContainer>;
-}
-
-export default UserHobby;
-
-const UserHobbyContainer = styled.div``;
