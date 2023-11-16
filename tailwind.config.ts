3772af49d53cf2fc485a6c2b726f334aa740fe8e
JitHoon <imessi0336@gmail.com>
2023-11-16 17:36:17 +0900

    Fix: 배포 스타일 적용 에러 해결 테스트

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,8 @@ const config: Config = withMT({
 		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./components/**/*.{js,ts,jsx,tsx,mdx}',
 		'./app/**/*.{js,ts,jsx,tsx,mdx}',
-		'path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
-		'path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
+		'./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
+		'./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
 		screens: {
