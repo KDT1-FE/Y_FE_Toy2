a395ac3ab6647ea8b9f97de7c0803964801adb2e
jisu Seo <jseo9732@gmail.com>
2023-11-17 13:02:39 +0900

    Fix

--- components/Common/index.ts
+++ components/Common/index.ts
@@ -1,5 +1,5 @@
 import Header from './Header1/Header1';
 import Modal from './Modal/Modal';
-import Navigation from './Navigation/Navigation';
+import Navigation from './Navigation1/Navigation1';
 
 export { Header, Modal, Navigation };
