5d829c06f176c5118957b5ebcf8c76d070efdb38
jisu Seo <jseo9732@gmail.com>
2023-11-17 10:52:18 +0900

    Fix

--- components/Chat/index.ts
+++ components/Chat/index.ts
@@ -1,8 +1,8 @@
 import ChatLoading from './ChatLoading/ChatLoading';
 import ChatAlert from './ChatAlert';
 import ChatroomHeader from './ChatroomHeader';
-import EntryNotice from './EntryNotice';
-import ExitNotice from './ExitNotice';
+import EntryNotice from './EntryNotice1';
+import ExitNotice from './ExitNotice1';
 import MyMessage from './MyMessage';
 import OtherMessage from './OtherMessage';
 
