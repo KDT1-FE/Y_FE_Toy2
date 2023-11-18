b9249652a2359444327c0d52a6008b7c2c3f4091
JiHongkyu <hongkyu9941@gmail.com>
2023-11-18 16:11:19 +0900

    Feat: 날짜별 채팅 정렬 기능 추가

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -4,6 +4,7 @@ import 'dayjs/locale/ko';
 import { MessageType } from '../../types/MessageType';
 import * as S from '../../styles/chat/MessageStyles';
 import { UserInfo } from '../../hooks/useGetUserInfo';
+import DatePlaceholder from './DatePlaceholder';
 
 interface IMessageProps {
   userInfo: UserInfo | null;
@@ -15,10 +16,11 @@ interface IMessageProps {
 function Message({ message, messages, userInfo, users }: IMessageProps) {
   const [prevMessage, setPrevMessage] = useState<MessageType | null>(null);
   const [nextMessage, setNextMessage] = useState<MessageType | null>(null);
-  const formattedDate = dayjs(message.createdAt).locale('ko').format('A h:mm');
-  const NextMsgFormattedDate = dayjs(nextMessage?.createdAt)
+  const formattedTime = dayjs(message.createdAt).locale('ko').format('A h:mm');
+  const NextMsgFormattedTime = dayjs(nextMessage?.createdAt)
     .locale('ko')
     .format('A h:mm');
+
   const sender = users.find((user) => user.id === message.userId);
   let isSentByCurrentUser = false;
 
@@ -33,40 +35,46 @@ function Message({ message, messages, userInfo, users }: IMessageProps) {
     setNextMessage(messages[MessageIndex + 1]);
   }, [messages]);
 
-  return isSentByCurrentUser ? (
-    <S.MessageContainer style={{ alignItems: 'flex-end' }}>
-      <S.MessageWrapper style={{ justifyContent: 'flex-end' }}>
-        <S.Date>
-          {(NextMsgFormattedDate !== formattedDate ||
-            nextMessage?.userId !== message.userId) &&
-            formattedDate}
-        </S.Date>
-        <S.TextBox sx={{ backgroundColor: 'primary.main' }}>
-          <S.MessageText>{message?.text}</S.MessageText>
-        </S.TextBox>
-      </S.MessageWrapper>
-    </S.MessageContainer>
-  ) : (
-    <S.MessageContainer sx={{ alignItems: 'flex-start' }}>
-      {prevMessage?.userId !== message.userId && (
-        <S.SenderWrapper>
-          <S.ImageWrapper>
-            <S.SenderImage src={sender?.picture} alt="보낸사람 이미지" />
-          </S.ImageWrapper>
-          <S.SenderName>{sender?.name}</S.SenderName>
-        </S.SenderWrapper>
+  return (
+    <S.Wrapper>
+      <DatePlaceholder prevMessage={prevMessage} message={message} />
+
+      {isSentByCurrentUser ? (
+        <S.MessageContainer style={{ alignItems: 'flex-end' }}>
+          <S.MessageWrapper style={{ justifyContent: 'flex-end' }}>
+            <S.Date>
+              {(NextMsgFormattedTime !== formattedTime ||
+                nextMessage?.userId !== message.userId) &&
+                formattedTime}
+            </S.Date>
+            <S.TextBox sx={{ backgroundColor: 'primary.main' }}>
+              <S.MessageText>{message?.text}</S.MessageText>
+            </S.TextBox>
+          </S.MessageWrapper>
+        </S.MessageContainer>
+      ) : (
+        <S.MessageContainer sx={{ alignItems: 'flex-start' }}>
+          {prevMessage?.userId !== message.userId && (
+            <S.SenderWrapper>
+              <S.ImageWrapper>
+                <S.SenderImage src={sender?.picture} alt="보낸사람 이미지" />
+              </S.ImageWrapper>
+              <S.SenderName>{sender?.name}</S.SenderName>
+            </S.SenderWrapper>
+          )}
+          <S.MessageWrapper>
+            <S.TextBox sx={{ backgroundColor: 'secondary.main' }}>
+              <S.MessageText>{message?.text}</S.MessageText>
+            </S.TextBox>
+            <S.Date>
+              {(NextMsgFormattedTime !== formattedTime ||
+                nextMessage?.userId !== message.userId) &&
+                formattedTime}
+            </S.Date>
+          </S.MessageWrapper>
+        </S.MessageContainer>
       )}
-      <S.MessageWrapper>
-        <S.TextBox sx={{ backgroundColor: 'secondary.main' }}>
-          <S.MessageText>{message?.text}</S.MessageText>
-        </S.TextBox>
-        <S.Date>
-          {(NextMsgFormattedDate !== formattedDate ||
-            nextMessage?.userId !== message.userId) &&
-            formattedDate}
-        </S.Date>
-      </S.MessageWrapper>
-    </S.MessageContainer>
+    </S.Wrapper>
   );
 }
 
